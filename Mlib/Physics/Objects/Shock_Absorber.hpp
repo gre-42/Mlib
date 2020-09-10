f7e590eac75df2ab6fd1de9f98fbd380f7b6000c
Your Name <you@example.com>
2020-09-10 17:42:33 +0200

    semi implicit absorber

--- Mlib/Physics/Objects/Shock_Absorber.hpp
+++ Mlib/Physics/Objects/Shock_Absorber.hpp
@@ -4,9 +4,14 @@ namespace Mlib {
 
 class ShockAbsorber {
 public:
+    enum class IntegrationMode {
+        IMPLICIT,
+        EXPLICIT,
+        SEMI_IMPLICIT
+    };
     ShockAbsorber(float Ks, float Ka);
     void integrate_force(float f);
-    void advance_time(float dt, bool implicit = true);
+    void advance_time(float dt, IntegrationMode integration_mode = IntegrationMode::SEMI_IMPLICIT);
     inline float position() const {
         return position_;
     }
