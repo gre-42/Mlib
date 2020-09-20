8eaf4802e3e10e7b42cf7064dbb64b7d0c535d61
Your Name <you@example.com>
2020-09-20 14:37:15 +0200

    hist opt

--- Mlib/Stats/Histogram.hpp
+++ Mlib/Stats/Histogram.hpp
@@ -12,15 +12,16 @@ void histogram(const Array<TData>& data, Array<size_t>& hist, Array<TData>& bins
     Array<TData> fdata = data[isfinite(data)];
     TData mi = min(fdata);
     TData ma = max(fdata);
-    TData h = (ma - mi) / (nbins - 1);
-    Array<TData> boundaries = linspace(mi - h / 2, ma + h / 2, nbins + 1);
+    // TData h = (ma - mi) / (nbins - 1);
+    // Array<TData> boundaries = linspace(mi - h / 2, ma + h / 2, nbins + 1);
     hist = zeros<size_t>(ArrayShape{nbins});
     for(size_t i = 0; i < fdata.length(); ++i) {
-        for(size_t j = 1; j < boundaries.length(); ++j) {
-            if (const TData& v = fdata(i); v >= boundaries(j - 1) && v < boundaries(j)) {
-                ++hist(j - 1);
-            }
-        }
+        // for(size_t j = 1; j < boundaries.length(); ++j) {
+        //     if (const TData& v = fdata(i); v >= boundaries(j - 1) && v < boundaries(j)) {
+        //         ++hist(j - 1);
+        //     }
+        // }
+        ++hist(((nbins - 1) * (fdata(i) - mi)) / (ma - mi));
     }
     bins = linspace(mi, ma, nbins);
 }
