442549eb41216ca7856bafc3c68d5cfe99eec4ae
Your Name <you@example.com>
2020-09-20 16:08:55 +0200

    simplify

--- Apps/Match_Histograms/Match_Histograms.cpp
+++ Apps/Match_Histograms/Match_Histograms.cpp
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
     StbInfo iref = stb_load(args.named_value("--ref"), false, false);
     Array<unsigned char> image = stb_image_2_array(iimage);
     Array<unsigned char> ref = stb_image_2_array(iref);
-    Array<unsigned char> out = histogram_matching(image.flattened().casted<float>(), ref.flattened().casted<float>(), 256).casted<unsigned char>();
+    Array<unsigned char> out = histogram_matching<unsigned char, unsigned char, float>(image.flattened(), ref.flattened(), 256);
     out.reshape(image.shape());
     std::unique_ptr<unsigned char> iout{new unsigned char[image.nelements()]};
     std::cerr << out[0][100].casted<float>() << std::endl;
