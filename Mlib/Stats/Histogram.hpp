28e6bc1bf5f4a4abeb6593a6c7dcc93f3bf5e19b
Your Name <you@example.com>
2020-09-20 15:03:10 +0200

    hist class

--- Mlib/Stats/Histogram.hpp
+++ Mlib/Stats/Histogram.hpp
@@ -7,23 +7,45 @@
 namespace Mlib {
 
 template <class TData>
-void histogram(const Array<TData>& data, Array<size_t>& hist, Array<TData>& bins, size_t nbins = 10) {
-    assert(nbins > 1);
-    Array<TData> fdata = data[isfinite(data)];
-    TData mi = min(fdata);
-    TData ma = max(fdata);
-    // TData h = (ma - mi) / (nbins - 1);
-    // Array<TData> boundaries = linspace(mi - h / 2, ma + h / 2, nbins + 1);
-    hist = zeros<size_t>(ArrayShape{nbins});
-    for(size_t i = 0; i < fdata.length(); ++i) {
-        // for(size_t j = 1; j < boundaries.length(); ++j) {
-        //     if (const TData& v = fdata(i); v >= boundaries(j - 1) && v < boundaries(j)) {
-        //         ++hist(j - 1);
-        //     }
-        // }
-        ++hist(((nbins - 1) * (fdata(i) - mi)) / (ma - mi));
+class Histogram {
+public:
+    Histogram(const Array<TData>& data, size_t nbins = 10) {
+        assert(nbins > 1);
+        Array<TData> fdata = data[isfinite(data)];
+        mi_ = min(fdata);
+        ma_ = max(fdata);
+        // TData h = (ma - mi) / (nbins - 1);
+        // Array<TData> boundaries = linspace(mi - h / 2, ma + h / 2, nbins + 1);
+        hist_ = zeros<size_t>(ArrayShape{nbins});
+        for(const TData& v : fdata.flat_iterable()) {
+            // for(size_t j = 1; j < boundaries.length(); ++j) {
+            //     if (const TData& v = fdata(i); v >= boundaries(j - 1) && v < boundaries(j)) {
+            //         ++hist(j - 1);
+            //     }
+            // }
+            ++hist_(bin_id(v));
+        }
     }
-    bins = linspace(mi, ma, nbins);
+    const Array<size_t>& hist() {
+        return hist_;
+    }
+    Array<TData> bins() {
+        return linspace(mi_, ma_, hist_.length());
+    }
+    size_t bin_id(const TData& data) {
+        return ((hist_.length() - 1) * (data - mi_)) / (ma_ - mi_);
+    }
+private:
+    Array<size_t> hist_;
+    TData mi_;
+    TData ma_;
+};
+
+template <class TData>
+void histogram(const Array<TData>& data, Array<size_t>& hist, Array<TData>& bins, size_t nbins = 10) {
+    Histogram hm{data, nbins};
+    hist = hm.hist();
+    bins = hm.bins();
 }
 
 }
