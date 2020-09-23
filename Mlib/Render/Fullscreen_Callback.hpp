e769b483988cc7bd62bcb4d3892ddf6f3e91715c
Your Name <you@example.com>
2020-09-23 17:31:05 +0200

    default w/h

--- Mlib/Render/Fullscreen_Callback.hpp
+++ Mlib/Render/Fullscreen_Callback.hpp
@@ -7,8 +7,8 @@ namespace Mlib {
 struct BaseUserObject {
     int window_x = 0;
     int window_y = 0;
-    int window_width = 0;
-    int window_height = 0;
+    int window_width = 640;
+    int window_height = 480;
 };
 
 void fullscreen_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
