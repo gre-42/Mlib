5dbeaeb3806c306e5a0fb4170c7bb22b5834eaa5
Your Name <you@example.com>
2020-10-21 19:09:45 +0200

    uniform_random_array

--- Mlib/Stats/Random_Arrays.hpp
+++ Mlib/Stats/Random_Arrays.hpp
@@ -4,16 +4,16 @@
 
 namespace Mlib {
 
-template <class TFloat>
-Array<std::complex<TFloat>> uniform_random_complex_array(const ArrayShape& shape, size_t seed) {
-    auto ar = random_array<std::complex<TFloat>>(shape, seed);
-    auto ai = random_array<std::complex<TFloat>>(shape, seed + 1);
-    return ar + std::complex<TFloat>(0, 1) * ai;
-}
-
 /*
  * Uniformly random array
  */
+template <class TFloat>
+Array<std::complex<TFloat>> uniform_random_complex_array(const ArrayShape& shape, size_t seed) {
+    auto ar = uniform_random_array<std::complex<TFloat>>(shape, seed);
+    auto ai = uniform_random_array<std::complex<TFloat>>(shape, seed + 1);
+    return ar + std::complex<TFloat>(0, 1) * ai;
+}
+
 template <class TDerived, class TData>
 void randomize_array_uniform(BaseDenseArray<TDerived, TData>& a, unsigned int seed) {
     UniformRandomNumberGenerator<TData> r{seed};
@@ -32,6 +32,13 @@ Array<TData> uniform_random_array(const ArrayShape& shape, unsigned int seed) {
 /*
  * Normal random array
  */
+template <class TFloat>
+Array<std::complex<TFloat>> normal_random_complex_array(const ArrayShape& shape, size_t seed) {
+    auto ar = normal_random_array<std::complex<TFloat>>(shape, seed);
+    auto ai = normal_random_array<std::complex<TFloat>>(shape, seed + 1);
+    return ar + std::complex<TFloat>(0, 1) * ai;
+}
+
 template <class TDerived, class TData>
 void randomize_array_normal(BaseDenseArray<TDerived, TData>& a, unsigned int seed) {
     NormalRandomNumberGenerator<TData> r{seed};
