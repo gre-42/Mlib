9b25348cfd46e2eaef8a6b60f543cda84e6716e4
Your Name <you@example.com>
2020-11-04 23:06:11 +0100

    nsprings = 100

--- Mlib/Scene/Load_Scene.cpp
+++ Mlib/Scene/Load_Scene.cpp
@@ -648,7 +648,7 @@ void LoadScene::operator()(
                 // Ds = 1000 / 4 * 9.8 / 0.02 = 122500 = 1.225e5
 
                 // Da * 1 = 1000 / 4 * 9.8 => Da = 1e4 / 4
-                size_t nsprings = 10;
+                size_t nsprings = 100;
                 float max_dist = 0.05;
                 auto tp = rb->tires_.insert({
                     tire_id,
