9332e0024a122ed4549040dcca4ae92e5a58322d
Your Name <you@example.com>
2020-11-12 17:06:26 +0100

    split

--- Mlib/Physics/Handle_Tire_Triangle_Intersection.cpp
+++ Mlib/Physics/Handle_Tire_Triangle_Intersection.cpp
@@ -4,6 +4,72 @@
 
 using namespace Mlib;
 
+void accelerate_positive(
+    RigidBody& rb,
+    float power,
+    const FixedArray<float, 3>& v3,
+    const FixedArray<float, 3>& n3,
+    float force_n1,
+    const PhysicsEngineConfig& cfg,
+    size_t tire_id)
+{
+    float v_max = 400.f / 3.6f;
+    // r = v / w
+    float f = 1;
+    if (float vc = sum(squared(rb.rbi_.v_)); vc > 1e-12) {
+        if (float vt = sum(squared(rb.get_velocity_at_tire(tire_id))); vt > 1e-12) {
+            f = std::clamp<float>(std::sqrt(vt / vc), 1e-1, 1e1);
+        }
+    }
+    float vv;
+    for (vv = 0; vv >= -v_max; vv -= 0.1) {
+        FixedArray<float, 3> tf = friction_force_infinite_mass(
+            cfg.stiction_coefficient * force_n1,
+            cfg.friction_coefficient * force_n1,
+            v3 + n3 * vv,
+            cfg.alpha0 / cfg.oversampling);
+        if (dot0d(tf, n3) > std::abs(power / vv) * f) {
+            break;
+        }
+    }
+    rb.set_tire_angular_velocity(tire_id, vv / rb.get_tire_radius(tire_id));
+}
+
+void accelerate_negative(
+    RigidBody& rb,
+    float power,
+    const FixedArray<float, 3>& v3,
+    const FixedArray<float, 3>& n3,
+    float force_n1,
+    const PhysicsEngineConfig& cfg,
+    size_t tire_id)
+{
+    float v_max = 400.f / 3.6f;
+    // r = v / w
+    float f = 1;
+    if (float vc = sum(squared(rb.rbi_.v_)); vc > 1e-12) {
+        if (float vt = sum(squared(rb.get_velocity_at_tire(tire_id))); vt > 1e-12) {
+            f = std::clamp<float>(std::sqrt(vt / vc), 1e-1, 1e1);
+        }
+    }
+    float vv;
+    for (vv = 0; vv <= v_max; vv += 0.1) {
+        FixedArray<float, 3> tf = friction_force_infinite_mass(
+            cfg.stiction_coefficient * force_n1,
+            cfg.friction_coefficient * force_n1,
+            v3 + n3 * vv,
+            cfg.alpha0 / cfg.oversampling);
+        if (-dot0d(tf, n3) > std::abs(power / vv) * f) {
+            break;
+        }
+    }
+    rb.set_tire_angular_velocity(tire_id, vv / rb.get_tire_radius(tire_id));
+}
+
+void idle(RigidBody& rb, size_t tire_id) {
+    rb.set_tire_angular_velocity(tire_id, rb.get_angular_velocity_at_tire(tire_id));
+}
+
 FixedArray<float, 3> Mlib::handle_tire_triangle_intersection(
     RigidBody& rb,
     const FixedArray<float, 3>& v3,
@@ -13,10 +79,8 @@ FixedArray<float, 3> Mlib::handle_tire_triangle_intersection(
     size_t tire_id)
 {
     PowerIntent P = rb.consume_tire_surface_power(tire_id);
-    float r = rb.get_tire_radius(tire_id);
     // F = W / s = W / v / t = P / v
     if (!std::isnan(P.power)) {
-        float v_max = 400.f / 3.6f;
         // std::cerr << "dx " << dx << std::endl;
         bool slipping = false;
         if ((P.power != 0) && !slipping) {
@@ -25,58 +89,22 @@ FixedArray<float, 3> Mlib::handle_tire_triangle_intersection(
                 rb.set_tire_angular_velocity(tire_id, 0);
             } else if (P.power > 0) {
                 if (P.type == PowerIntentType::BREAK_OR_IDLE) {
-                    rb.set_tire_angular_velocity(tire_id, rb.get_angular_velocity_at_tire(tire_id));
+                    idle(rb, tire_id);
                 } else {
-                    // r = v / w
-                    float f = 1;
-                    if (float vc = sum(squared(rb.rbi_.v_)); vc > 1e-12) {
-                        if (float vt = sum(squared(rb.get_velocity_at_tire(tire_id))); vt > 1e-12) {
-                            f = std::clamp<float>(std::sqrt(vt / vc), 1e-1, 1e1);
-                        }
-                    }
-                    float vv;
-                    for (vv = 0; vv >= -v_max; vv -= 0.1) {
-                        FixedArray<float, 3> tf = friction_force_infinite_mass(
-                            cfg.stiction_coefficient * force_n1,
-                            cfg.friction_coefficient * force_n1,
-                            v3 + n3 * vv,
-                            cfg.alpha0 / cfg.oversampling);
-                        if (dot0d(tf, n3) > std::abs(P.power / vv) * f) {
-                            break;
-                        }
-                    }
-                    rb.set_tire_angular_velocity(tire_id, vv / r);
+                    accelerate_positive(rb, P.power, v3, n3, force_n1, cfg, tire_id);
                 }
             } else if (P.power < 0) {
                 if (P.type == PowerIntentType::BREAK_OR_IDLE) {
-                    rb.set_tire_angular_velocity(tire_id, rb.get_angular_velocity_at_tire(tire_id));
+                    idle(rb, tire_id);
                 } else {
-                    // r = v / w
-                    float f = 1;
-                    if (float vc = sum(squared(rb.rbi_.v_)); vc > 1e-12) {
-                        if (float vt = sum(squared(rb.get_velocity_at_tire(tire_id))); vt > 1e-12) {
-                            f = std::clamp<float>(std::sqrt(vt / vc), 1e-1, 1e1);
-                        }
-                    }
-                    float vv;
-                    for (vv = 0; vv <= v_max; vv += 0.1) {
-                        FixedArray<float, 3> tf = friction_force_infinite_mass(
-                            cfg.stiction_coefficient * force_n1,
-                            cfg.friction_coefficient * force_n1,
-                            v3 + n3 * vv,
-                            cfg.alpha0 / cfg.oversampling);
-                        if (-dot0d(tf, n3) > std::abs(P.power / vv) * f) {
-                            break;
-                        }
-                    }
-                    rb.set_tire_angular_velocity(tire_id, vv / r);
+                    accelerate_negative(rb, P.power, v3, n3, force_n1, cfg, tire_id);
                 }
             }
         } else if (P.power == 0) {
-            rb.set_tire_angular_velocity(tire_id, rb.get_angular_velocity_at_tire(tire_id));
+            idle(rb, tire_id);
         }
     }
-    float v1 = rb.get_tire_angular_velocity(tire_id) * r;
+    float v1 = rb.get_tire_angular_velocity(tire_id) * rb.get_tire_radius(tire_id);
     return friction_force_infinite_mass(
         cfg.stiction_coefficient * force_n1,
         cfg.friction_coefficient * force_n1,
