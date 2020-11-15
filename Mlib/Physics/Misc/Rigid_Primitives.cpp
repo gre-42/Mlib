448aa81ad84c580aa65e98c7dbb4c2d798e55ac8
Your Name <you@example.com>
2020-11-15 17:08:41 +0100

    fix rbp

--- Mlib/Physics/Misc/Rigid_Primitives.cpp
+++ Mlib/Physics/Misc/Rigid_Primitives.cpp
@@ -65,36 +65,6 @@ RigidBodyIntegrator Mlib::rigid_cuboid_integrator(
     };
 }
 
-RigidBodyIntegrator Mlib::rigid_cuboid_integrator(
-    float mass,
-    const FixedArray<float, 3>& size,
-    const FixedArray<float, 3>& com)
-{
-    // From: https://de.wikipedia.org/wiki/Steinerscher_Satz
-    FixedArray<float, 3, 3> I{
-        1.f / 12 * mass * (squared(size(1)) + squared(size(2))), 0, 0,
-        0, 1.f / 12 * mass * (squared(size(0)) + squared(size(2))), 0,
-        0, 0, 1.f / 12 * mass * (squared(size(0)) + squared(size(1)))};
-    
-    if (mass != INFINITY) {
-        FixedArray<float, 3, 3> a = cross(com);
-        I += mass * dot2d(a.T(), a);
-    }
-
-    return RigidBodyIntegrator{
-        mass,
-        fixed_zeros<float, 3>(),            // L
-        I,                                  // I
-        com,                                // com
-        fixed_zeros<float, 3>(),            // v
-        fixed_zeros<float, 3>(),            // w
-        fixed_zeros<float, 3>(),            // M
-        fixed_nans<float, 3>(),             // position
-        fixed_nans<float, 3>(),             // rotation
-        count_nonzero(com != 0.f) <= 1      // I_is_diagonal
-    };
-}
-
 std::shared_ptr<RigidBody> Mlib::rigid_cuboid(
     RigidBodies& rigid_bodies,
     float mass,
