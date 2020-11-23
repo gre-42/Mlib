e715f416a59fc7a3ac7b11bb9c758da42fd54de6
Your Name <you@example.com>
2020-11-23 23:25:34 +0100

    max_extra_friction arg

--- Apps/Render_Scene_File/Render_Scene_File.cpp
+++ Apps/Render_Scene_File/Render_Scene_File.cpp
@@ -61,6 +61,7 @@ int main(int argc, char** argv) {
         "[--friction_coefficient <x>] "
         "[--alpha0 <x>] "
         "[--lateral_stability <x>] "
+        "[--max_extra_friction <x>] "
         "[--no_avoid_burnout] "
         "[--wheel_penetration_depth <x>]"
         "[--print_fps] "
@@ -117,6 +118,7 @@ int main(int argc, char** argv) {
          "--friction_coefficient",
          "--alpha0",
          "--lateral_stability",
+         "--max_extra_friction",
          "--wheel_penetration_depth"});
     try {
         const auto args = parser.parsed(argc, argv);
@@ -188,6 +190,7 @@ int main(int argc, char** argv) {
                 .alpha0 = safe_stof(args.named_value("--alpha0", "0.1")),
                 .avoid_burnout = !args.has_named("--no_avoid_burnout"),
                 .lateral_stability = safe_stof(args.named_value("--lateral_stability")),
+                .max_extra_friction = safe_stof(args.named_value("--max_extra_friction")),
                 .wheel_penetration_depth = safe_stof(args.named_value("--wheel_penetration_depth", "0.25")),
                 .physics_type = physics_type_from_string(args.named_value("--physics_type", "version1")),
                 .resolve_collision_type = resolve_collission_type_from_string(args.named_value("--resolve_collisions_type", "penalty")),
