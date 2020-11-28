e941b5f6d5b1560fc003d0d2542498f5f18e67c4
Your Name <you@example.com>
2020-11-28 17:30:38 +0100

    CombinedMagicFormula

--- Mlib/Physics/Collision/Magic_Formula.hpp
+++ Mlib/Physics/Collision/Magic_Formula.hpp
@@ -1,7 +1,39 @@
 #pragma once
 #include <cmath>
+#include <Mlib/Array/Fixed_Array.hpp>
 
 namespace Mlib {
+
+template <class TData>
+struct MagicFormula {
+    TData B = 41;
+    TData C = 1.4;
+    TData D = 1;
+    TData E = -0.2;
+    TData operator () (const TData& x) const {
+        return D * std::sin(C * std::atan(B * x - E * (B * x - std::atan(B * x))));
+    }
+    TData argmax() const {
+        return 1 / (B * std::sqrt(E - 1));
+    }
+};
+
+/**
+ * From: Brian Beckman, The Physics Of Racing Series, Part 25
+ */
+template <class TData>
+struct CombinedMagicFormula {
+    FixedArray<MagicFormula<TData>, 2> f;
+    FixedArray<TData, 2> operator () (const FixedArray<TData, 2>& x) const {
+        FixedArray<TData, 2> s{
+            x(0) / f(0).argmax(),
+            x(1) / f(1).argmax()};
+        TData p = std::sqrt(sum(squared(s)));
+        return {
+            s(0) / p * f(0)(x(0) * p * f(0).argmax()),
+            s(1) / p * f(1)(x(1) * p * f(1).argmax())};
+    }
+};
 
 /**
  * From: https://en.wikipedia.org/wiki/Hans_B._Pacejka
@@ -17,7 +49,7 @@ TData magic_formula(
     const TData& D = 1,
     const TData& E = -0.2)
 {
-    return D * std::sin(C * std::atan(B * x - E * (B * x - std::atan(B * x))));
+    return MagicFormula<TData>{.B = B, .C = C, .D = D, .E = E}(x);
 }
 
 }
