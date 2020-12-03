c599528e9f6c5097aef04ec983837dc5ad151e30
Your Name <you@example.com>
2020-12-03 21:58:24 +0100

    histmatch ref fix

--- Mlib/Images/Match_Rgba_Histograms.cpp
+++ Mlib/Images/Match_Rgba_Histograms.cpp
@@ -6,6 +6,12 @@ using namespace Mlib;
 
 Array<unsigned char> Mlib::match_rgba_histograms(const Array<unsigned char>& image, const Array<unsigned char>& ref) {
     Array<unsigned char> out{image.shape()};
+    if (image.ndim() != 3) {
+        throw std::runtime_error("Image does not have 3 dimensions");
+    }
+    if (ref.ndim() != 3) {
+        throw std::runtime_error("Reference image does not have 3 dimensions");
+    }
     if (image.shape(0) != 3 && image.shape(0) != 4) {
         throw std::runtime_error("Image does not have 3 or 4 channels");
     }
@@ -18,7 +24,9 @@ Array<unsigned char> Mlib::match_rgba_histograms(const Array<unsigned char>& ima
         }
     } else if (image.shape(0) == 4) {
         Array<bool> mask = (image[3] > (unsigned char)50);
-        Array<bool> mask_ref = (ref[3] > (unsigned char)50);
+        Array<bool> mask_ref = ref.shape(0) == 3
+            ? ones<bool>(ref.shape().erased_first())
+            : (ref[3] > (unsigned char)50);
         for(size_t d = 0; d < 3; ++d) {
             HistogramMatching<unsigned char, unsigned char, float> hm{image[d][mask], ref[d][mask_ref], 256};
             for(size_t r = 0; r < image.shape(1); ++r) {
