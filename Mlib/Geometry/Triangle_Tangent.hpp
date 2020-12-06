c4fca5719a048e087b1c77bbbc1f60a19f99317a
Your Name <you@example.com>
2020-12-06 17:24:22 +0100

    simplify triangle_tangent

--- Mlib/Geometry/Triangle_Tangent.hpp
+++ Mlib/Geometry/Triangle_Tangent.hpp
@@ -15,17 +15,14 @@ FixedArray<TData, 3> triangle_tangent(
     const FixedArray<TData, 2>& uv1,
     const FixedArray<TData, 2>& uv2)
 {
-    FixedArray<TData, 3> edge1 = pos1 - pos0;
-    FixedArray<TData, 3> edge2 = pos2 - pos0;
+    FixedArray<TData, 3> edge0 = pos1 - pos0;
+    FixedArray<TData, 3> edge1 = pos2 - pos0;
     FixedArray<TData, 2> deltaUV0 = uv1 - uv0;
     FixedArray<TData, 2> deltaUV1 = uv2 - uv0;
 
     TData f = 1.0f / (deltaUV0(0) * deltaUV1(1) - deltaUV1(0) * deltaUV0(1));
 
-    FixedArray<TData, 3> tangent1{
-        f * (deltaUV1(1) * edge1(0) - deltaUV0(1) * edge2(0)),
-        f * (deltaUV1(1) * edge1(1) - deltaUV0(1) * edge2(1)),
-        f * (deltaUV1(1) * edge1(2) - deltaUV0(1) * edge2(2))};
+    FixedArray<TData, 3> tangent1 = f * (deltaUV1(1) * edge0 - deltaUV0(1) * edge1);
     tangent1 /= std::sqrt(sum(squared(tangent1)));
     return tangent1;
 }
