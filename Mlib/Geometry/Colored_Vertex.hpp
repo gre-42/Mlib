8b61b517a6d4aa0ce820ef4ce3a3b0ee4d460d35
Your Name <you@example.com>
2020-12-06 16:52:14 +0100

    colored vertex transformed

--- Mlib/Geometry/Colored_Vertex.hpp
+++ Mlib/Geometry/Colored_Vertex.hpp
@@ -1,8 +1,12 @@
 #pragma once
-#include <Mlib/Array/Fixed_Array.hpp>
+#include <Mlib/Geometry/Homogeneous.hpp>
+#include <Mlib/Math/Fixed_Math.hpp>
 #include <iosfwd>
 
 namespace Mlib {
+
+#pragma GCC push_options
+#pragma GCC optimize ("O3")
 
 struct ColoredVertex {
     FixedArray<float, 3> position;
@@ -10,7 +14,18 @@ struct ColoredVertex {
     FixedArray<float, 2> uv;
     FixedArray<float, 3> normal;
     FixedArray<float, 3> tangent;
+
+    ColoredVertex transformed(const FixedArray<float, 4, 4>& m) const {
+        return ColoredVertex{
+            .position = dehomogenized_3(dot1d(m, homogenized_4(position))),
+            .color = color,
+            .uv = uv,
+            .normal = dot1d(R3_from_4x4(m), normal),
+            .tangent = dot1d(R3_from_4x4(m), tangent)};
+    }
 };
+
+#pragma GCC pop_options
 
 inline std::ostream& operator << (std::ostream& ostr, const ColoredVertex& v) {
     ostr << "p " << v.position << " n " << v.normal << " c " << v.color << " t " << v.uv;
