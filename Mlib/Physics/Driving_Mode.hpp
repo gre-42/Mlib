7205dcfc138133c2c7c7a11d2d0c159414bf36c6
Your Name <you@example.com>
2020-12-29 00:33:59 +0100

    city no correct

--- Mlib/Physics/Driving_Mode.hpp
+++ Mlib/Physics/Driving_Mode.hpp
@@ -23,7 +23,7 @@ static std::map<std::string, DrivingMode> driving_modes{
         .max_velocity = 30 / 3.6,
         .max_delta_velocity2_break = 2,
         .collision_avoidance_radius_break = 6,
-        .collision_avoidance_radius_correct = 15,
+        .collision_avoidance_radius_correct = 0,
         .collision_avoidance_cos = 0.6,
         .collision_avoidance_delta = 0.5,
         .stuck_velocity = 2 / 3.6,
