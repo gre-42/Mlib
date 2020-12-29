5271d9d2f5de5c21b1e5f5af08413fb9544247a4
Your Name <you@example.com>
2020-12-29 18:14:38 +0100

    stb_image_load.cpp

---
+++ stb_image/stb_image_load.cpp
@@ -0,0 +1,32 @@
+#include "stb_image_load.h"
+
+void stb_image_flip_horizontally(const StbInfo& image) {
+    for (size_t r = 0; r < (size_t)image.height; ++r) {
+        for (size_t c = 0; c < (size_t)image.width / 2; ++c) {
+            for (size_t d = 0; d < (size_t)image.nrChannels; ++d) {
+                std::swap(
+                    image.data.get()[(r * image.width  + c) * image.nrChannels + d],
+                    image.data.get()[(r * image.width  + image.width - 1 - c) * image.nrChannels + d]);
+            }
+        }
+    }
+}
+
+StbInfo stb_load(const std::string& filename, bool flip_vertically, bool flip_horizontally) {
+    StbInfo result;
+
+    stbi_set_flip_vertically_on_load_thread(flip_vertically);
+    result.data.reset(stbi_load(
+            filename.c_str(),
+            &result.width,
+            &result.height,
+            &result.nrChannels,
+            0));
+    if (result.data == nullptr) {
+        throw std::runtime_error("Could not load \"" + filename + '"');
+    }
+    if (flip_horizontally) {
+        stb_image_flip_horizontally(result);
+    }
+    return result;
+}
--- stb_image/stb_image_load.h
+++ stb_image/stb_image_load.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <stb_image/stb_image.h>
+#include "stb_image.h"
 #include <memory>
 #include <string>
 
@@ -10,33 +10,6 @@ struct StbInfo {
     std::unique_ptr<unsigned char, decltype(&stbi_image_free)> data{nullptr, &stbi_image_free};
 };
 
-static void stb_image_flip_horizontally(const StbInfo& image) {
-    for (size_t r = 0; r < (size_t)image.height; ++r) {
-        for (size_t c = 0; c < (size_t)image.width / 2; ++c) {
-            for (size_t d = 0; d < (size_t)image.nrChannels; ++d) {
-                std::swap(
-                    image.data.get()[(r * image.width  + c) * image.nrChannels + d],
-                    image.data.get()[(r * image.width  + image.width - 1 - c) * image.nrChannels + d]);
-            }
-        }
-    }
-}
+void stb_image_flip_horizontally(const StbInfo& image);
 
-static StbInfo stb_load(const std::string& filename, bool flip_vertically, bool flip_horizontally) {
-    StbInfo result;
-
-    stbi_set_flip_vertically_on_load_thread(flip_vertically);
-    result.data.reset(stbi_load(
-            filename.c_str(),
-            &result.width,
-            &result.height,
-            &result.nrChannels,
-            0));
-    if (result.data == nullptr) {
-        throw std::runtime_error("Could not load \"" + filename + '"');
-    }
-    if (flip_horizontally) {
-        stb_image_flip_horizontally(result);
-    }
-    return result;
-}
+StbInfo stb_load(const std::string& filename, bool flip_vertically, bool flip_horizontally);
