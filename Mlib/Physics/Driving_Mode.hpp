25a0b2e4ef3d8dcd64b4e617de458c93490364f7
Your Name <you@example.com>
2021-01-01 20:36:53 +0100

    city no break

--- Mlib/Physics/Driving_Mode.hpp
+++ Mlib/Physics/Driving_Mode.hpp
@@ -22,7 +22,7 @@ static std::map<std::string, DrivingMode> driving_modes{
         .rest_radius = 5,
         .max_velocity = 30 / 3.6,
         .max_delta_velocity2_break = 2,
-        .collision_avoidance_radius_break = 6,
+        .collision_avoidance_radius_break = 0,
         .collision_avoidance_radius_correct = 0,
         .collision_avoidance_cos = 0.6,
         .collision_avoidance_delta = 0.5,
