564aabf6260fdf8aa75cb4051d97812566578ea9
Your Name <you@example.com>
2021-01-09 15:56:59 +0100

    opt absolutify

--- Mlib/Geometry/Mesh/Bone.cpp
+++ Mlib/Geometry/Mesh/Bone.cpp
@@ -8,18 +8,22 @@ std::vector<OffsetAndQuaternion<float>> Bone::absolutify(
 {
     std::vector<OffsetAndQuaternion<float>> result;
     result.resize(transformations.size());
+#ifndef NDEBUG
     for (OffsetAndQuaternion<float>& r : result) {
         r.offset() = NAN;
     }
+#endif
     absolutify(
         transformations,
         OffsetAndQuaternion<float>::identity(),
         result);
+#ifndef NDEBUG
     for (const OffsetAndQuaternion<float>& r : result) {
         if (any(isnan(r.offset()))) {
             throw std::runtime_error("Bone transformation contains NAN values");
         }
     }
+#endif
     return result;
 }
 
@@ -28,12 +32,14 @@ void Bone::absolutify(
     const OffsetAndQuaternion<float>& parent_transformation,
     std::vector<OffsetAndQuaternion<float>>& result)
 {
+#ifndef NDEBUG
     if (index >= result.size()) {
         throw std::runtime_error("Bone index too large for result array");
     }
     if (index >= transformations.size()) {
         throw std::runtime_error("Bone index too large for transformations");
     }
+#endif
     const OffsetAndQuaternion<float>& m = initial_absolute_transformation;
     OffsetAndQuaternion<float> n = parent_transformation * transformations[index];
     result[index] = n * m.inverse();
