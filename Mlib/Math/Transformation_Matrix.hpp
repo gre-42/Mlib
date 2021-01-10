dd145c87854b4a60e76b3fd84b2f31069c237f00
Your Name <you@example.com>
2021-01-11 00:28:36 +0100

    inline + O3 transformation

--- Mlib/Math/Transformation_Matrix.hpp
+++ Mlib/Math/Transformation_Matrix.hpp
@@ -4,75 +4,78 @@
 #include <cmath>
 #include <iostream>
 
+#pragma GCC push_options
+#pragma GCC optimize ("O3")
+
 namespace Mlib {
 
 template <class TData>
 class TransformationMatrix {
 public:
-    static TransformationMatrix identity() {
+    static inline TransformationMatrix identity() {
         return TransformationMatrix{
             fixed_identity_array<TData, 3>(),
             fixed_zeros<TData, 3>()};
     }
 
-    static TransformationMatrix inverse(const FixedArray<TData, 3, 3>& R, const FixedArray<float, 3>& t) {
+    static inline TransformationMatrix inverse(const FixedArray<TData, 3, 3>& R, const FixedArray<float, 3>& t) {
         TransformationMatrix result;
         invert_t_R(t, R, result.t_, result.R_);
         return result;
     }
 
-    TransformationMatrix()
+    inline TransformationMatrix()
     {}
 
-    TransformationMatrix(const FixedArray<TData, 3, 3>& R, const FixedArray<float, 3>& t)
+    inline TransformationMatrix(const FixedArray<TData, 3, 3>& R, const FixedArray<float, 3>& t)
     : R_{R},
       t_{t}
     {}
 
-    explicit TransformationMatrix(const FixedArray<TData, 4, 4>& m)
+    inline explicit TransformationMatrix(const FixedArray<TData, 4, 4>& m)
     : R_{R3_from_4x4(m)},
       t_{t3_from_4x4(m)}
     {}
 
-    FixedArray<TData, 3> operator * (const FixedArray<TData, 3>& rhs) const {
+    inline FixedArray<TData, 3> operator * (const FixedArray<TData, 3>& rhs) const {
         return dot1d(R_, rhs) + t_;
     }
 
-    TransformationMatrix operator * (const TransformationMatrix& rhs) const {
+    inline TransformationMatrix operator * (const TransformationMatrix& rhs) const {
         return TransformationMatrix{
             dot2d(R_, rhs.R_),
             dot1d(R_, rhs.t_) + t_};
     }
 
-    FixedArray<TData, 3> rotate(const FixedArray<TData, 3>& rhs) const {
+    inline FixedArray<TData, 3> rotate(const FixedArray<TData, 3>& rhs) const {
         return dot1d(R_, rhs);
     }
 
-    const FixedArray<TData, 3, 3>& R() const {
+    inline const FixedArray<TData, 3, 3>& R() const {
         return R_;
     }
 
-    const FixedArray<TData, 3>& t() const {
+    inline const FixedArray<TData, 3>& t() const {
         return t_;
     }
 
-    FixedArray<TData, 3, 3>& R() {
+    inline FixedArray<TData, 3, 3>& R() {
         return R_;
     }
 
-    FixedArray<TData, 3>& t() {
+    inline FixedArray<TData, 3>& t() {
         return t_;
     }
 
-    const FixedArray<TData, 4, 4> affine() const {
+    inline const FixedArray<TData, 4, 4> affine() const {
         return assemble_homogeneous_4x4(R_, t_);
     }
 
-    TransformationMatrix inverted() const {
+    inline TransformationMatrix inverted() const {
         return inverse(R_, t_);
     }
 
-    TransformationMatrix inverted_scaled() const {
+    inline TransformationMatrix inverted_scaled() const {
         auto scale2 = sum(squared(R_)) / 3;
         return inverse(R_ / scale2, t_);
     }
@@ -82,3 +85,5 @@ private:
 };
 
 }
+
+#pragma GCC pop_options
