b7803327f8378352ef2482b180cb3bbafd3bcadd
Your Name <you@example.com>
2021-01-10 19:03:08 +0100

    two lane spawn points

--- Mlib/Render/Renderables/Renderable_Osm_Map/Calculate_Spawn_Points.cpp
+++ Mlib/Render/Renderables/Renderable_Osm_Map/Calculate_Spawn_Points.cpp
@@ -15,8 +15,6 @@ void Mlib::calculate_spawn_points(
     DrivingDirection driving_direction)
 {
     for (const auto& r : street_rectangles) {
-        SpawnPoint sp;
-        sp.location = r.location;
         FixedArray<float, 3> x = r.rectangle(0, 0) - r.rectangle(0, 1);
         FixedArray<float, 3> y = r.rectangle(0, 0) - r.rectangle(1, 0);
         {
@@ -34,34 +32,46 @@ void Mlib::calculate_spawn_points(
             x /= std::sqrt(sum(squared(x)));
         }
         FixedArray<float, 3> z = cross(x, y);
-        FixedArray<float, 3, 3> R{
+        FixedArray<float, 3, 3> R0{
             x(0), y(0), z(0),
             x(1), y(1), z(1),
             x(2), y(2), z(2)};
-        sp.rotation = matrix_2_tait_bryan_angles(R);
-        auto create_spawn_point = [&spawn_points, &sp, &r](SpawnPointType spawn_point_type, float alpha){
-            sp.type = spawn_point_type;
-            sp.position = alpha * (r.rectangle(0, 0) + r.rectangle(1, 0)) / 2.f + (1 - alpha) * (r.rectangle(0, 1) + r.rectangle(1, 1)) / 2.f;
-            spawn_points.push_back(sp);
+        auto r0 = matrix_2_tait_bryan_angles(R0);
+        auto r1 = matrix_2_tait_bryan_angles(dot2d(rodrigues(z, float(M_PI)), R0));
+        auto create_spawn_point = [&spawn_points, &r](
+            SpawnPointType spawn_point_type,
+            float alpha,
+            const FixedArray<float, 3>& rotation)
+        {
+            spawn_points.push_back(SpawnPoint{
+                .type = spawn_point_type,
+                .location = r.location,
+                .position = alpha * (r.rectangle(0, 0) + r.rectangle(1, 0)) / 2.f + (1 - alpha) * (r.rectangle(0, 1) + r.rectangle(1, 1)) / 2.f,
+                .rotation = rotation});
         };
         if (driving_direction == DrivingDirection::CENTER) {
-            create_spawn_point(SpawnPointType::ROAD, 0.5);
+            create_spawn_point(SpawnPointType::ROAD, 0.5, r0);
         } else if (driving_direction == DrivingDirection::LEFT) {
             if (r.nlanes == 2) {
-                create_spawn_point(SpawnPointType::ROAD, 1 - 0.5 - 0.25);
+                create_spawn_point(SpawnPointType::ROAD, 0.5 - 0.25, r0);
+                create_spawn_point(SpawnPointType::ROAD, 0.5 + 0.25, r1);
             } else if (r.nlanes == 4) {
-                create_spawn_point(SpawnPointType::ROAD, 1 - 0.5 - 0.125);
-                create_spawn_point(SpawnPointType::PARKING, 1 - 0.75 - 0.125);
+                create_spawn_point(SpawnPointType::ROAD, 0.5 - 0.125, r0);
+                create_spawn_point(SpawnPointType::PARKING, 0.5 - 0.25 - 0.125, r0);
+                create_spawn_point(SpawnPointType::ROAD, 0.5 + 0.125, r1);
+                create_spawn_point(SpawnPointType::PARKING, 0.5 + 0.25 + 0.125, r1);
             } else {
                 throw std::runtime_error("Unsupported number of lanes");
             }
         } else if (driving_direction == DrivingDirection::RIGHT) {
-            create_spawn_point(SpawnPointType::ROAD, 0.75);
             if (r.nlanes == 2) {
-                create_spawn_point(SpawnPointType::ROAD, 0.5 + 0.25);
+                create_spawn_point(SpawnPointType::ROAD, 0.5 + 0.25, r0);
+                create_spawn_point(SpawnPointType::ROAD, 0.5 - 0.25, r1);
             } else if (r.nlanes == 4) {
-                create_spawn_point(SpawnPointType::ROAD, 0.5 + 0.125);
-                create_spawn_point(SpawnPointType::PARKING, 0.75 + 0.125);
+                create_spawn_point(SpawnPointType::ROAD, 0.5 + 0.125, r0);
+                create_spawn_point(SpawnPointType::PARKING, 0.5 + 0.25 + 0.125, r0);
+                create_spawn_point(SpawnPointType::ROAD, 0.5 - 0.125, r1);
+                create_spawn_point(SpawnPointType::PARKING, 0.5 - 0.25 - 0.125, r1);
             } else {
                 throw std::runtime_error("Unsupported number of lanes");
             }
