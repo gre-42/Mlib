9671af182d59a60a91eee5d3e4544710e84387aa
Your Name <you@example.com>
2021-01-20 18:02:07 +0100

    b0 fix

--- Mlib/Physics/Collision/Constraints.cpp
+++ Mlib/Physics/Collision/Constraints.cpp
@@ -251,7 +251,7 @@ TireContactInfo1::TireContactInfo1(
 void TireContactInfo1::solve(float dt, float relaxation) {
     float force_min;
     float force_max;
-    FixedArray<float, 3> tv = updated_tire_speed(P_, rb_, b0_, vc_, n3_, v0_, fci_.normal_impulse().normal, cfg_, tire_id_, force_min, force_max);
+    FixedArray<float, 3> tv = updated_tire_speed(P_, rb_, b0_, vc_, n3_, v0_, fci_.normal_impulse().normal, cfg_, tire_id_, force_min, force_max) - b0_;
     // {
     //     FixedArray<float, 3> v3 = rb_.rbi_.rbp_.velocity_at_position(rb_.get_abs_tire_contact_position(tire_id_)) - v;
     //     v3 -= fci_.normal_impulse().normal * dot0d(v3, fci_.normal_impulse().normal);
