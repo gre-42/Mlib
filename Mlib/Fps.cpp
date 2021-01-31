57b88765b298e1e82873c3f3f9e20f4d9d2b1223
Klaus Sembritzki <klausem@gmail.com>
2021-01-31 11:44:45 +0100

    last_fps

--- Mlib/Fps.cpp
+++ Mlib/Fps.cpp
@@ -7,17 +7,22 @@ using namespace Mlib;
 
 Fps::Fps()
 : last_time_{std::chrono::steady_clock::now()},
+  last_fps_{NAN},
   mean_fps_{0},
   mad_fps_{0}
 {}
 
 void Fps::tick() {
     std::chrono::time_point current_time = std::chrono::steady_clock::now();
-    int64_t delta_time = std::chrono::duration_cast<std::chrono::microseconds>(current_time - last_time_).count();
-    float current_fps = (1e6f / delta_time);
-    mean_fps_ = (1 - fps_alpha) * mean_fps_ + fps_alpha * current_fps;
-    mad_fps_ = (1 - fps_alpha) * mad_fps_ + fps_alpha * std::abs(current_fps - mean_fps_);
+    std::chrono::duration<double> delta_time = (current_time - last_time_);
+    last_fps_ = 1 / (float)delta_time.count();
+    mean_fps_ = (1 - fps_alpha) * mean_fps_ + fps_alpha * last_fps_;
+    mad_fps_ = (1 - fps_alpha) * mad_fps_ + fps_alpha * std::abs(last_fps_ - mean_fps_);
     last_time_ = current_time;
+}
+
+float Fps::last_fps() const {
+    return last_fps_;
 }
 
 float Fps::mean_fps() const {
--- Mlib/Fps.hpp
+++ Mlib/Fps.hpp
@@ -7,10 +7,12 @@ class Fps {
 public:
     Fps();
     void tick();
+    float last_fps() const;
     float mean_fps() const;
     float mad_fps() const;
 private:
     std::chrono::steady_clock::time_point last_time_;
+    float last_fps_;
     float mean_fps_;
     float mad_fps_;
 };
