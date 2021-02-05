52f7b8d4102abbbc19bbeadb12ab07a47967e801
Klaus Sembritzki <klausem@gmail.com>
2021-02-05 16:26:08 +0100

    load obj fix

--- Mlib/Geometry/Mesh/Load_Obj.cpp
+++ Mlib/Geometry/Mesh/Load_Obj.cpp
@@ -230,6 +230,7 @@ std::list<std::shared_ptr<ColoredVertexArray>> Mlib::load_obj(
                 if (!tl.triangles_.empty()) {
                     result.push_back(tl.triangle_array());
                     tl.triangles_.clear();
+                    tl.material_.textures.clear();
                 }
                 tl.name_ = match[1].str();
             } else if (Mlib::re::regex_match(line, match, mtllib_reg)) {
