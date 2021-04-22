ce1fdca28a0708b23cef486777e9f1c16bf1d13a
Your Name <you@example.com>
2021-04-22 19:43:55 +0200

    tree random angle

--- Mlib/Render/Resources/Osm_Map_Resource/Add_Trees_To_Tree_Nodes.cpp
+++ Mlib/Render/Resources/Osm_Map_Resource/Add_Trees_To_Tree_Nodes.cpp
@@ -19,12 +19,13 @@ void Mlib::add_trees_to_tree_nodes(
     float scale)
 {
     NormalRandomNumberGenerator<float> scale_rng{0, 1.f, 0.2f};
+    UniformRandomNumberGenerator<float> yangle_rng{1, 0.f, 2 * float{M_PI}};
     for (const auto& n : nodes) {
         const auto& tags = n.second.tags;
         if (tags.contains("natural", "tree")) {
             const auto& p = n.second.position;
             if (std::isnan(min_dist_to_road) || !ground_bvh.has_neighbor(p, min_dist_to_road * scale)) {
-                add_parsed_resource_name(p, 0.f, rnc(), 0.f, scale_rng(), resource_instance_positions, object_resource_descriptors, hitboxes);
+                add_parsed_resource_name(p, 0.f, rnc(), yangle_rng(), scale_rng(), resource_instance_positions, object_resource_descriptors, hitboxes);
                 steiner_points.push_back({
                     .position = {p(0), p(1), 0.f},
                     .type = SteinerPointType::TREE_NODE,
