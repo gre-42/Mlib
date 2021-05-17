0601b5bb27af5b0c9233e32381e7201cbb01829d
Klaus Sembritzki <klausem@gmail.com>
2021-05-17 20:22:57 +0200

    aniso 16->8

--- Mlib/Render/Render_Config.hpp
+++ Mlib/Render/Render_Config.hpp
@@ -28,7 +28,7 @@ struct RenderConfig {
     bool window_maximized = false;
     bool full_screen = false;
     bool double_buffer = false;
-    unsigned int anisotropic_filtering_level = 16;
+    unsigned int anisotropic_filtering_level = 8;
     bool normalmaps = true;
     bool show_mouse_cursor = false;
     int swap_interval = 1;
