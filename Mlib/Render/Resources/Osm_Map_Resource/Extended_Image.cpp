c0b95a856f38ebc4139719259d79588214fd7e9d
Your Name <you@example.com>
2021-07-05 21:06:33 +0200

    preserve_original

--- Mlib/Render/Resources/Osm_Map_Resource/Extended_Image.cpp
+++ Mlib/Render/Resources/Osm_Map_Resource/Extended_Image.cpp
@@ -13,6 +13,10 @@ ExtendedImage::ExtendedImage(
     bool preserve_original)
 : extension_{ extension }
 {
+    if (extension == 0 && ((box_filter_radius == 0) || preserve_original)) {
+        extended_image_ = image;
+        return;
+    }
     if (image.ndim() != 2) {
         throw std::runtime_error("Dim not 2");
     }
@@ -21,29 +25,25 @@ ExtendedImage::ExtendedImage(
     }
     // This check is sufficient for the extension,
     // but not for points in the interior
-    if (niterations * box_filter_radius < extension) {
+    if (niterations * box_filter_radius < std::max({ extension, image.shape(0), image.shape(1) })) {
         throw std::runtime_error("niterations * box_filter < extension");
     }
-    if (extension == 0 && !mask.initialized() && (box_filter_radius == 0)) {
-        extended_image_ = image;
-    } else {
-        extended_image_ = nans<float>(image.shape() + 2 * extension);
-        for (size_t i = 0; i < niterations; ++i) {
-            for (size_t r = 0; r < image.shape(0); ++r) {
-                for (size_t c = 0; c < image.shape(1); ++c) {
-                    if (mask(r, c)) {
-                        extended_image_(r + extension_, c + extension_) = image(r, c);
-                    }
+    extended_image_ = nans<float>(image.shape() + 2 * extension);
+    for (size_t i = 0; i < niterations; ++i) {
+        for (size_t r = 0; r < image.shape(0); ++r) {
+            for (size_t c = 0; c < image.shape(1); ++c) {
+                if (mask(r, c)) {
+                    extended_image_(r + extension_, c + extension_) = image(r, c);
                 }
             }
-            extended_image_ = box_filter_nans_as_zeros_NWE(extended_image_, ArrayShape{ 2 * box_filter_radius + 1, 2 * box_filter_radius + 1 });
         }
-        if (preserve_original) {
-            for (size_t r = 0; r < image.shape(0); ++r) {
-                for (size_t c = 0; c < image.shape(1); ++c) {
-                    if (mask(r, c)) {
-                        extended_image_(r + extension_, c + extension_) = image(r, c);
-                    }
+        extended_image_ = box_filter_nans_as_zeros_NWE(extended_image_, ArrayShape{ 2 * box_filter_radius + 1, 2 * box_filter_radius + 1 });
+    }
+    if (preserve_original) {
+        for (size_t r = 0; r < image.shape(0); ++r) {
+            for (size_t c = 0; c < image.shape(1); ++c) {
+                if (mask(r, c)) {
+                    extended_image_(r + extension_, c + extension_) = image(r, c);
                 }
             }
         }
