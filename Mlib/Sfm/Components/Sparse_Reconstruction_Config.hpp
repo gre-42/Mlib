1e9ccf6a9d1dcc870a248204dd9f99ca884e5863
Your Name <you@example.com>
2021-07-24 14:36:20 +0200

    update config

--- Mlib/Sfm/Components/Sparse_Reconstruction_Config.hpp
+++ Mlib/Sfm/Components/Sparse_Reconstruction_Config.hpp
@@ -9,8 +9,8 @@ namespace Mlib { namespace Sfm {
 struct ReconstructionConfig {
     size_t npoints = 10;
     size_t nframes = 40;
-    bool recompute_first_camera = true;
-    bool recompute_second_camera = true;
+    bool recompute_first_camera = false;
+    bool recompute_second_camera = false;
     size_t recompute_interval = 5;
 
     bool print_residual = false;
@@ -19,7 +19,7 @@ struct ReconstructionConfig {
     float bad_point_residual_multiplier = 4.f;
     float max_residual_normalized = 0.1f;
     float max_residual_unnormalized = 10;
-    float max_residual_unnormalized_post_l2 = 30;
+    float max_residual_unnormalized_post_l2 = 5;
 
     // float projector_scale = 1.f; // 1.f
     bool use_ransac_append = false;
@@ -28,9 +28,9 @@ struct ReconstructionConfig {
     bool exclude_bad_points = true;
     bool clear_all_cameras = true;
     bool two_pass = false;
-    bool initialize_with_bundle_adjustment = true;
+    bool initialize_with_bundle_adjustment = false;
     bool interpolate_initial_cameras = true;
-    bool append_with_bundle_adjustment = true;
+    bool append_with_bundle_adjustment = false;
     GlobalBundleConfig gb {
         .numerical_jacobian_x = false,
         .numerical_jacobian_k = false
@@ -41,17 +41,17 @@ struct ReconstructionConfig {
         .marginalization_target = MarginalizationTarget::POINTS
     };
     RansacOptions<float> ro_initial {
-        .nelems_small = 20,
-        .ncalls = 10,
-        .inlier_distance_thresh = squared(10.f),
-        .inlier_count_thresh = 10,
+        .nelems_small = 8,
+        .ncalls = 100,
+        .inlier_distance_thresh = squared(5.f),
+        .inlier_count_thresh = 20,
         .seed = 1
     };
     RansacOptions<float> ro_append {
-        .nelems_small = 10,
-        .ncalls = 10,
-        .inlier_distance_thresh = 1.f,
-        .inlier_count_thresh = 10,
+        .nelems_small = 8,
+        .ncalls = 100,
+        .inlier_distance_thresh = squared(5.f),
+        .inlier_count_thresh = 20,
         .seed = 1
     };
 };
