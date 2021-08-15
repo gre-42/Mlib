c37c292c45de33f614b55d731803fc9272a0a8d7
Your Name <you@example.com>
2021-08-15 17:04:55 +0200

    mean axis

--- Mlib/Stats/Mean.hpp
+++ Mlib/Stats/Mean.hpp
@@ -11,6 +11,13 @@ TData mean(const BaseDenseArray<TDerived, TData>& a) {
     return sum(a) / (ScalarType)a->nelements();
 }
 
+template <class TData>
+Array<TData> mean(const Array<TData>& a, size_t axis) {
+    assert(a.shape(axis) > 0);
+    typedef typename ScalarType<TData>::value_type ScalarType;
+    return sum(a, axis) / (ScalarType)a.shape(axis);
+}
+
 template <class TDerived, class TData>
 TData nanmean(const BaseDenseArray<TDerived, TData>& a) {
     return mean((*a)[!Mlib::isnan(a)]);
