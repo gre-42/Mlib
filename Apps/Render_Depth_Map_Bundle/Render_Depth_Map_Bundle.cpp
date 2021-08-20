4f07a7e969e0b0f5e276f74b9f1cb1838aaea0a3
Your Name <you@example.com>
2021-08-20 19:16:24 +0200

    depth bundle double buffering

--- Apps/Render_Depth_Map_Bundle/Render_Depth_Map_Bundle.cpp
+++ Apps/Render_Depth_Map_Bundle/Render_Depth_Map_Bundle.cpp
@@ -68,7 +68,8 @@ int main(int argc, char** argv) {
         RenderConfig render_config{
             .wire_frame = args.has_named("--wire_frame"),
             .screen_width = (int)ref->second.depth.shape(1),
-            .screen_height = (int)ref->second.depth.shape(0)};
+            .screen_height = (int)ref->second.depth.shape(0),
+            .double_buffer = true};
         SceneNodeResources scene_node_resources;
         RenderingContextGuard rrg{scene_node_resources, "primary_rendering_resources", render_config.anisotropic_filtering_level, 0};
         RenderResults render_results;
