208d5fb7e0e8b70ea7dd5fafab59299222ed3024
Your Name <you@example.com>
2021-12-31 00:08:05 +0100

    Update vec hull

--- Mlib/Players/Pod_Bot_Mlib_Compat/vectorial_constants.hpp
+++ Mlib/Players/Pod_Bot_Mlib_Compat/vectorial_constants.hpp
@@ -2,7 +2,7 @@
 #include <Mlib/Players/Pod_Bot_Mlib_Compat/types.hpp>
 
 static const ::Vector g_vecZero{0.f, 0.f, 0.f};
-static const ::Vector VEC_HULL_MIN{0.f, 0.f, 1.8f * 60.f};
-static const ::Vector VEC_DUCK_HULL_MIN{0.f, 0.f, 1.f * 60.f};
+static const ::Vector VEC_HULL_MIN{-16.f, -16.f, 0.f};
+static const ::Vector VEC_DUCK_HULL_MIN{-16.f, -16.f, 0.f};
 static const ::Vector VIEW_OFS{ 0.f, 0.f, 50.f };
-static const ::Vector WAYPOINT_OFS{ 0.f, 0.f, 1 * 60.f};
+static const ::Vector WAYPOINT_OFS{ 0.f, 0.f, 60.f};
