b49782677fe77d4e0ea67eba31413d2738d25d31
Your Name <you@example.com>
2022-03-06 17:42:32 +0100

    Cleanup SaveTextureAtlasPng

--- Mlib/Scene/Load_Scene_Functions/Resources/Save_Texture_Atlas_Png.cpp
+++ Mlib/Scene/Load_Scene_Functions/Resources/Save_Texture_Atlas_Png.cpp
@@ -9,6 +9,14 @@
 
 using namespace Mlib;
 
+#define BEGIN_OPTIONS static size_t option_id = 1
+#define DECLARE_OPTION(a) static const size_t a = option_id++
+
+BEGIN_OPTIONS;
+DECLARE_OPTION(NAME);
+DECLARE_OPTION(FILENAME);
+DECLARE_OPTION(COLOR_MODE);
+
 LoadSceneUserFunction SaveTextureAtlasPng::user_function = [](const LoadSceneUserFunctionArgs& args)
 {
     static DECLARE_REGEX(regex,
@@ -29,17 +37,21 @@ void SaveTextureAtlasPng::execute(
     const std::smatch& match,
     const LoadSceneUserFunctionArgs& args)
 {
+    std::string filename = match[FILENAME].str();
+    if (!filename.ends_with(".png")) {
+        throw std::runtime_error("Filename \"" + filename + "\" does not end with .png");
+    }
     StbInfo img = RenderingContextStack::primary_rendering_resources()->get_texture_data(TextureDescriptor{
-        .color = match[1].str(),
-        .color_mode = color_mode_from_string(match[3].str())});
+        .color = match[NAME].str(),
+        .color_mode = color_mode_from_string(match[COLOR_MODE].str())});
     if (!stbi_write_png(
-        match[2].str().c_str(),
+        filename.c_str(),
         img.width,
         img.height,
         img.nrChannels,
         img.data.get(),
         0))
     {
-        throw std::runtime_error("Could not write \"" + match[2].str() + '"');
+        throw std::runtime_error("Could not write \"" + filename + '"');
     }
 }
