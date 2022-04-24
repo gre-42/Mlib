99856133c1dc585d849f1ed601dd364ebec25d6b
Marc Kremer <marc.kremer@polyfit42.com>
2022-04-24 19:47:03 +0200

    Cleanup parsing

--- Mlib/Scene_Graph/Render_Pass.cpp
+++ Mlib/Scene_Graph/Render_Pass.cpp
@@ -8,6 +8,10 @@ ExternalRenderPassType Mlib::external_render_pass_type_from_string(const std::st
         return ExternalRenderPassType::NONE;
     } else if (str == "standard") {
         return ExternalRenderPassType::STANDARD;
+    } else if (str == "dirtmap") {
+        return ExternalRenderPassType::DIRTMAP;
+    } else if (str == "lightmap_depth") {
+        return ExternalRenderPassType::LIGHTMAP_DEPTH;
     } else if (str == "lightmap_global_static") {
         return ExternalRenderPassType::LIGHTMAP_GLOBAL_STATIC;
     } else if (str == "lightmap_global_dynamic") {
@@ -20,8 +24,6 @@ ExternalRenderPassType Mlib::external_render_pass_type_from_string(const std::st
         return ExternalRenderPassType::LIGHTMAP_BLACK_NODE;
     } else if (str == "lightmap_black_global_and_local") {
         return ExternalRenderPassType::LIGHTMAP_BLACK_GLOBAL_AND_LOCAL;
-    } else if (str == "dirtmap") {
-        return ExternalRenderPassType::DIRTMAP;
     } else {
         throw std::runtime_error("Unknown render pass type: \"" + str + '"');
     }
@@ -30,13 +32,14 @@ ExternalRenderPassType Mlib::external_render_pass_type_from_string(const std::st
 std::string Mlib::external_render_pass_type_to_string(ExternalRenderPassType pass) {
     switch(pass) {
         case ExternalRenderPassType::STANDARD: return "standard";
+        case ExternalRenderPassType::DIRTMAP: return "dirtmap";
+        case ExternalRenderPassType::LIGHTMAP_DEPTH: return "lightmap_depth";
         case ExternalRenderPassType::LIGHTMAP_GLOBAL_STATIC: return "lightmap_global_static";
         case ExternalRenderPassType::LIGHTMAP_GLOBAL_DYNAMIC: return "lightmap_global_dynamic";
         case ExternalRenderPassType::LIGHTMAP_BLACK_GLOBAL_STATIC: return "lightmap_black_global_static";
         case ExternalRenderPassType::LIGHTMAP_BLACK_LOCAL_INSTANCES: return "lightmap_black_local_instances";
         case ExternalRenderPassType::LIGHTMAP_BLACK_NODE: return "lightmap_black_node";
         case ExternalRenderPassType::LIGHTMAP_BLACK_GLOBAL_AND_LOCAL: return "lightmap_black_global_and_local";
-        case ExternalRenderPassType::DIRTMAP: return "dirtmap";
         default:
             throw std::runtime_error("Unknown render pass type");
     }
