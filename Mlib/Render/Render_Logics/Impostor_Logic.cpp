8e1de9c99548b27f04bb8a0fa7d38a0deeef88bf
Marc Kramer <marc.kramer@polyfit42.com>
2022-09-10 15:02:40 +0200

    Fix cos dist

--- Mlib/Render/Render_Logics/Impostor_Logic.cpp
+++ Mlib/Render/Render_Logics/Impostor_Logic.cpp
@@ -1,11 +1,14 @@
 #include "Impostor_Logic.hpp"
 #include <Mlib/Geometry/Coordinates/Gl_Look_At.hpp>
 #include <Mlib/Geometry/Material.hpp>
+#include <Mlib/Images/StbImage.hpp>
+#include <Mlib/Images/Vectorial_Pixels.hpp>
 #include <Mlib/Log.hpp>
 #include <Mlib/Math/Fixed_Rodrigues.hpp>
 #include <Mlib/Render/Aggregate_Array_Renderer.hpp>
 #include <Mlib/Render/Array_Instances_Renderer.hpp>
 #include <Mlib/Render/Array_Instances_Renderers.hpp>
+#include <Mlib/Render/CHK.hpp>
 #include <Mlib/Render/Cameras/Generic_Camera.hpp>
 #include <Mlib/Render/Instance_Handles/Frame_Buffer.hpp>
 #include <Mlib/Render/Instance_Handles/Render_Guards.hpp>
@@ -67,8 +70,12 @@ ImpostorLogic::ImpostorLogic(
     }
     orig_node.set_node_hider(orig_hider);
     Material material{
-        .blend_mode = BlendMode::CONTINUOUS,
-        .textures = { {.texture_descriptor = TextureDescriptor{.color = texture_id_}} }};
+        // .blend_mode = BlendMode::CONTINUOUS,
+        .textures = { {.texture_descriptor = TextureDescriptor{.color = texture_id_}} },
+        .emissivity = OrderableFixedArray<float, 3>{0.5f, 0.5f, 0.5f},
+        .ambience = OrderableFixedArray<float, 3>{2.f, 2.f, 2.f},
+        .diffusivity = OrderableFixedArray<float, 3>{0.f, 0.f, 0.f},
+        .specularity = OrderableFixedArray<float, 3>{0.f, 0.f, 0.f}};
     material.compute_color_mode();
     SquareResource res{
         FixedArray<float, 2, 2>{-10.f, 0.f, 10.f, 5.f},
@@ -113,9 +120,12 @@ void ImpostorLogic::render(
     auto camera_position = scene_.get_node(cameras_.camera_node_name()).absolute_model_matrix().t();
     auto renderable_position = orig_node_.absolute_model_matrix().t();
     auto dir_camera_to_renderable = renderable_position - camera_position;
-    if ((sum(squared(dir_camera_to_renderable)) > 1e-12) &&
-        ((fbs_ == nullptr) ||
-         (dot0d(dir_camera_to_renderable, old_dir_camera_to_renderable_) < 0.9)))
+    if (sum(squared(dir_camera_to_renderable)) < 1e-12) {
+        return;
+    }
+    dir_camera_to_renderable /= std::sqrt(sum(squared(dir_camera_to_renderable)));
+    if ((fbs_ == nullptr) ||
+        (dot0d(dir_camera_to_renderable, old_dir_camera_to_renderable_) < 0.9))
     {
         old_dir_camera_to_renderable_ = dir_camera_to_renderable;
         GLsizei impostor_texture_width = 1024;
@@ -123,12 +133,12 @@ void ImpostorLogic::render(
         ViewportGuard vg{0, 0, impostor_texture_width, impostor_texture_height};
         SceneNode impostor_camera_node;
         TransformationMatrix<float, double, 3> impostor_camera_model_matrix{
-            gl_lookat_absolute(camera_position, renderable_position).casted<float>(),
+            gl_lookat_relative(dir_camera_to_renderable).casted<float>(),
             camera_position};
         impostor_camera_node.set_relative_pose(camera_position, matrix_2_tait_bryan_angles(impostor_camera_model_matrix.R()), 1.f);
         impostor_camera_node.set_camera(
             std::make_unique<GenericCamera>(
-                CameraConfig{},
+                CameraConfig(),
                 GenericCamera::Postprocessing::DISABLED,
                 GenericCamera::Mode::PERSPECTIVE));
         RenderedSceneDescriptor impostor_rsd{.external_render_pass = {ExternalRenderPassType::IMPOSTOR_NODE, "", &orig_node_, &impostor_camera_node}, .time_id = 0};
@@ -149,17 +159,18 @@ void ImpostorLogic::render(
             InstancesRendererGuard irg{
                 std::make_shared<ArrayInstancesRenderers>(),
                 std::make_shared<ArrayInstancesRenderer>()};
-            child_logic_.render(impostor_texture_width, impostor_texture_height, render_config, scene_graph_config, render_results, impostor_rsd);
+            CHK(glClearColor(1.f, 0.f, 1.f, 1.f));
+            CHK(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
+            // child_logic_.render(impostor_texture_width, impostor_texture_height, render_config, scene_graph_config, render_results, impostor_rsd);
             // VectorialPixels<float, 3> vpx{ArrayShape{size_t(impostor_texture_width), size_t(impostor_texture_height)}};
             // CHK(glReadPixels(0, 0, impostor_texture_width, impostor_texture_height, GL_RGB, GL_FLOAT, vpx->flat_iterable().begin()));
-            // PpmImage::from_float_rgb(vpx.to_array()).save_to_file("/tmp/impostor.ppm");
+            // StbImage::from_float_rgb(vpx.to_array()).save_to_file("/tmp/impostor.png");
         }
 
         rendering_context_.rendering_resources->set_texture(texture_id_, fbs_->fb.texture_color);
         rendering_context_.rendering_resources->set_vp(texture_id_, vp());
         impostor_hider_.is_initialized = true;
     }
-    child_logic_.render(width, height, render_config, scene_graph_config, render_results, frame_id);
 }
 
 float ImpostorLogic::near_plane() const {
