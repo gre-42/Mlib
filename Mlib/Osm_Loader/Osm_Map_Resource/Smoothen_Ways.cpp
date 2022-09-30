6708e2e197e7f9521584da365578a849d8276463
Marc Kramer <marc.kramer@polyfit42.com>
2022-09-30 21:47:02 +0200

    Smoothen terrain regions

--- Mlib/Osm_Loader/Osm_Map_Resource/Smoothen_Ways.cpp
+++ Mlib/Osm_Loader/Osm_Map_Resource/Smoothen_Ways.cpp
@@ -27,31 +27,43 @@ NodesAndWays Mlib::smoothen_ways(
     float scale,
     float max_length)
 {
-    auto include_way = [&included_highways](const Way& way){
+    auto include_some_nodes = [](const Way& way) {
+        return way.tags.contains("terrain_region");
+    };
+    auto include_all_nodes = [&included_highways](const Way& way){
         return
             way.tags.contains("highway") &&
             included_highways.contains(way.tags.get("highway")) &&
             !way.tags.contains("smoothen", "no");
     };
+    auto include_node = [](const Node& node) {
+        return node.tags.contains("smoothen", "yes");
+    };
     std::map<std::string, std::set<std::string>> node_neighbors;
     for (const auto& [way_id, way] : naws.ways) {
-        if (!include_way(way)) {
+        bool include_all = include_all_nodes(way);
+        if (!include_all && !include_some_nodes(way)) {
             continue;
         }
         for (auto it = way.nd.begin(); it != way.nd.end(); ++it) {
             auto s = it;
             ++s;
-            if (s != way.nd.end()) {
-                node_neighbors[*it].insert(*s);
-                node_neighbors[*s].insert(*it);
+            if (s == way.nd.end()) {
+                break;
             }
+            if (!include_all && (!include_node(naws.nodes.at(*s)) || !include_node(naws.nodes.at(*it)))) {
+                continue;
+            }
+            node_neighbors[*it].insert(*s);
+            node_neighbors[*s].insert(*it);
         }
     }
     NodesAndWays result;
     result.nodes = naws.nodes;
     size_t segment_ctr = 0;
     for (const auto& [way_id, way] : naws.ways) {
-        if (!include_way(way)) {
+        bool include_all = include_all_nodes(way);
+        if (!include_all && !include_some_nodes(way)) {
             result.ways[way_id] = way;
             continue;
         }
@@ -63,33 +75,38 @@ NodesAndWays Mlib::smoothen_ways(
             if (i1 == way.nd.end()) {
                 break;
             }
+            const auto& nd0 = naws.nodes.at(*i0);
+            const auto& nd1 = naws.nodes.at(*i1);
+            if (!include_all && (!include_node(nd0) || !include_node(nd1))) {
+                continue;
+            }
             const auto& neighbors0 = node_neighbors.at(*i0);
             const auto& neighbors1 = node_neighbors.at(*i1);
             if ((neighbors0.size() > 2) || (neighbors1.size() > 2)) {
                 continue;
             }
-            if ((neighbors0.size() == 1) && (neighbors1.size() == 1)) {
+            if (include_all && ((neighbors0.size() == 1) && (neighbors1.size() == 1))) {
                 continue;
             }
             FixedArray<double, 2> n0;
             if (neighbors0.size() == 1) {
-                n0 = naws.nodes.at(*i1).position - naws.nodes.at(*i0).position;
+                n0 = nd1.position - nd0.position;
             } else {
                 auto nrs = neighbors0;
                 nrs.erase(*i1);
-                n0 = naws.nodes.at(*i1).position - naws.nodes.at(*nrs.begin()).position;
+                n0 = nd1.position - naws.nodes.at(*nrs.begin()).position;
             }
             n0 /= std::sqrt(sum(squared(n0)));
             FixedArray<double, 2> n1;
             if (neighbors1.size() == 1) {
-                n1 = naws.nodes.at(*i1).position - naws.nodes.at(*i0).position;
+                n1 = nd1.position - nd0.position;
             } else {
                 auto nrs = neighbors1;
                 nrs.erase(*i0);
-                n1 = naws.nodes.at(*nrs.begin()).position - naws.nodes.at(*i0).position;
+                n1 = naws.nodes.at(*nrs.begin()).position - nd0.position;
             }
             n1 /= std::sqrt(sum(squared(n1)));
-            double d = std::sqrt(sum(squared(naws.nodes.at(*i1).position - naws.nodes.at(*i0).position))) / 2.;
+            double d = std::sqrt(sum(squared(nd1.position - nd0.position))) / 2.;
             size_t n = 1 + size_t(d / scale / max_length);
             if (n <= 2) {
                 continue;
@@ -98,14 +115,16 @@ NodesAndWays Mlib::smoothen_ways(
             for (size_t i = 1; i < n - 1; ++i) {
                 auto snode_id = "snode_" + std::to_string(segment_ctr++);
                 auto snode_p = smooth_intermediate_node(
-                    naws.nodes.at(*i0).position,
-                    naws.nodes.at(*i1).position,
+                    nd0.position,
+                    nd1.position,
                     n0,
                     n1,
                     d,
                     t[i]);
                 new_nd.push_back(snode_id);
-                result.nodes[snode_id] = Node{.position = snode_p, .tags = {}};
+                auto tags = nd0.tags;
+                tags.insert(nd1.tags.begin(), nd1.tags.end());
+                result.nodes[snode_id] = Node{.position = snode_p, .tags = tags};
             }
         }
         result.ways[way_id] = Way{
