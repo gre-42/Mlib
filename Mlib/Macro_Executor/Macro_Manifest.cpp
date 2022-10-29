306ec360fbef297d8c460e680ba09f8bd9ba76de
Marc Kramer <marc.kramer@polyfit42.com>
2022-10-29 17:01:01 +0200

    Improve error handling

--- Mlib/Macro_Executor/Macro_Manifest.cpp
+++ Mlib/Macro_Executor/Macro_Manifest.cpp
@@ -9,19 +9,23 @@ using namespace Mlib;
 
 MacroManifest::MacroManifest(const std::string& filename) {
     if (filename.ends_with(".json")) {
-        nlohmann::json j;
-        std::ifstream ifs{filename};
-        if (ifs.fail()) {
-            throw std::runtime_error("Could not open file \"" + filename + '"');
+        try {
+            nlohmann::json j;
+            std::ifstream ifs{filename};
+            if (ifs.fail()) {
+                throw std::runtime_error("Could not open file \"" + filename + '"');
+            }
+            ifs >> j;
+            if (!ifs.eof() && ifs.fail()) {
+                throw std::runtime_error("Error reading from file: \"" + filename + '"');
+            }
+            for (const auto& [key, value] : j["variables"].get<std::map<std::string, std::string>>()) {
+                variables.insert(key, value);
+            }
+            script_file = (fs::path{filename}.parent_path() / j["script_file"]).string();
+        } catch (const nlohmann::json::exception& e) {
+            throw std::runtime_error("Error loading file \"" + filename + "\": " + e.what());
         }
-        ifs >> j;
-        if (!ifs.eof() && ifs.fail()) {
-            throw std::runtime_error("Error reading from file: \"" + filename + '"');
-        }
-        for (const auto& [key, value] : j["variables"].get<std::map<std::string, std::string>>()) {
-            variables.insert(key, value);
-        }
-        script_file = (fs::path{filename}.parent_path() / j["script_file"]).string();
     } else if (filename.ends_with(".scn")) {
         script_file = filename;
     }
