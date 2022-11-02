c6df3f157b8258579493c9ec6007e6ca6db561cd
Marc Kramer <marc.kramer@polyfit42.com>
2022-11-03 00:28:32 +0100

    Install OpenAL

--- build_freealut.ps1
+++ build_freealut.ps1
@@ -3,9 +3,10 @@ Push-Location $PSScriptRoot
 # 1. Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# Install CMake and OpenAL SDK
+# Install CMake, OpenAL SDK and OpenAL
 choco install -y cmake --installargs ADD_CMAKE_TO_PATH=System
 choco install -y openalsdk
+choco install -y openal
 
 # Invoke-WebRequest https://www.openal.org/downloads/OpenAL11CoreSDK.zip -OutFile OpenAL11CoreSDK.zip
 # Expand-Archive OpenAL11CoreSDK.zip -DestinationPath .\
