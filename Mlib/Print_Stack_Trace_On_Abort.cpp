2004df86dac8c71d03e92fae4cbbc168b1cd464d
Marc Kramer <marc.kramer@polyfit42.com>
2022-12-30 13:32:03 +0100

    Print_Stack_Trace_On_Abort

---
+++ Mlib/Print_Stack_Trace_On_Abort.cpp
@@ -0,0 +1,18 @@
+#include "Print_Stack_Trace_On_Abort.hpp"
+#include <Mlib/Os/Os.hpp>
+#include <Mlib/Stack_Trace.hpp>
+#include <csignal>
+
+using namespace Mlib;
+
+extern "C" void abort_handler(int signal_number)
+{
+    lerr() << "Abort called";
+    print_stacktrace();
+    lerr() << "Calling exit(134)";
+    std::exit(134);
+}
+
+void Mlib::print_stack_trace_on_abort() {
+    signal(SIGABRT, &abort_handler);
+}
---
+++ Mlib/Print_Stack_Trace_On_Abort.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace Mlib {
+
+void print_stack_trace_on_abort();
+
+}
