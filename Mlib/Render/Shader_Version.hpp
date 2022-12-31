655af682f31084191c309a498abe148e14a7e377
Marc Kramer <marc.kramer@polyfit42.com>
2022-12-31 15:34:00 +0100

    Increase shader version to 3.2

--- Mlib/Render/Shader_Version.hpp
+++ Mlib/Render/Shader_Version.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #ifdef __ANDROID__
-#define SHADER_VER "#version 310 es\n"
+#define SHADER_VER "#version 320 es\n"
 #define FRAGMENT_PRECISION "precision mediump float;\nprecision mediump int;\n"
 #else
 #define SHADER_VER "#version 330 core\n"
