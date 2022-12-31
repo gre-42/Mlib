c44101b1cf2bc6d442900218e4dc546a4f4d05a1
Marc Kramer <marc.kramer@polyfit42.com>
2022-12-31 15:04:47 +0100

    Simplify log

--- Mlib/Log.cpp
+++ Mlib/Log.cpp
@@ -1,13 +1,12 @@
 #include "Log.hpp"
 #include <Mlib/Env.hpp>
-#include <iostream>
+#include <Mlib/Os/Os.hpp>
 #include <thread>
 
 using namespace Mlib;
 
 THREAD_LOCAL(size_t) Log::level_ = 0;
 
-#ifdef __ANDROID__
 Log::Log(const std::string& message)
 {
     info(message);
@@ -25,34 +24,3 @@ void Log::info(const std::string& message) {
             std::string(level_ * 4, ' ') <<
             message;
 }
-#else
-Log::Log(const std::string& message, std::ostream* ostr)
-: ostr_{ostr}
-{
-    info(message);
-    ++level_;
-}
-
-Log::Log(const std::string& message, const std::string& env_var)
-: Log{
-    message,
-    (::Mlib::getenv_default_bool(env_var.c_str(), false) ||
-     ::Mlib::getenv_default_bool("ENABLE_LOG", false))
-        ? &std::cerr
-        : nullptr}
-{}
-
-Log::~Log() {
-    --level_;
-    info("done");
-}
-
-void Log::info(const std::string& message) {
-    if (ostr_ != nullptr) {
-        (*ostr_) <<
-            std::this_thread::get_id() << " " <<
-            std::string(level_ * 4, ' ') <<
-            message << std::endl;
-    }
-}
-#endif
--- Mlib/Log.hpp
+++ Mlib/Log.hpp
@@ -1,11 +1,9 @@
 #pragma once
 #include <Mlib/Threads/Thread_Local.hpp>
-#include <iostream>
 #include <string>
 
 namespace Mlib {
 
-#ifdef __ANDROID__
 class Log {
 public:
     explicit Log(const std::string& message);
@@ -14,18 +12,6 @@ public:
 private:
     static THREAD_LOCAL(size_t) level_;
 };
-#else
-class Log {
-public:
-    Log(const std::string& message, std::ostream* ostr = &std::cerr);
-    Log(const std::string& message, const std::string& env_var);
-    ~Log();
-    static void info(const std::string& message);
-private:
-    static THREAD_LOCAL(size_t) level_;
-    std::ostream* ostr_;
-};
-#endif
 
 #ifdef MLIB_ENABLE_LOG
 #define LOG_FUNCTION(msg) ::Mlib::Log log(msg)
