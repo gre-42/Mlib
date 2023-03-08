5448191209197852edaf6a2f8bdbd5e91b17ab99
Marc Kramer <marc.kramer@polyfit42.com>
2023-03-08 22:34:40 +0100

    Simplify build.cmd

--- build.cmd
+++ build.cmd
@@ -5,7 +5,7 @@ set BUILD_DIR=%BUILD_PREFIX%VS
 mkdir %BUILD_DIR%
 cd %BUILD_DIR% || exit /b
 set OPENALDIR=C:\Program Files (x86)\OpenAL 1.1 SDK
-cmake ../ %CMAKE_OPTIONS% || exit /b
+cmake .. %CMAKE_OPTIONS% || exit /b
 cmake ^
     --build . ^
     --config %CMAKE_BUILD_TYPE% ^
