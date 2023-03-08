a43c800df289536f3372a78de6d165286cf8e076
Marc Kramer <marc.kramer@polyfit42.com>
2023-03-08 21:08:26 +0100

    Correct build_sceene_renderer.cmd

--- build_scene_renderer.cmd
+++ build_scene_renderer.cmd
@@ -2,4 +2,4 @@ cd /D "%~dp0" || exit /b
 
 set BUILD_PREFIX=G
 set CMAKE_OPTIONS="-DBUILD_TRIANGLE=OFF -DBUILD_CV=OFF -DBUILD_SFM=OFF -DBUILD_OPENCV=OFF"
-Mlib\build.cmd || exit /b
+build.cmd || exit /b
