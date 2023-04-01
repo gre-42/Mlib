17d33df6c6921d06e313d5969af998cf2fcedac6
Marc Kramer <marc.kramer@polyfit42.com>
2023-04-01 14:19:55 +0200

    Remix args + nperiods fix

--- Mlib/Audio/Remix.py
+++ Mlib/Audio/Remix.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 import csv
+import json
+import os.path
+from argparse import ArgumentParser
 from itertools import groupby
 
-import matplotlib.pyplot as plt
 import numpy as np
 from scipy.io import wavfile
-from scipy.signal import stft
 
 
 def compute_offset(data0: np.ndarray, data1: np.ndarray) -> int:
@@ -19,24 +20,26 @@ def compute_offset(data0: np.ndarray, data1: np.ndarray) -> int:
 
 # print(compute_offset([0,1,0,0], [0,0,0,0,1,0]))
 
-def remix():
+def remix(args):
+    os.makedirs(args.out_dir, exist_ok=True)
     TIME0 = 0
     TIME1 = 1
     LABEL = 2
 
     NMIN = 100
-    NPERIODS_LEFT = 90
+    NPERIODS_LEFT = 80
     NPERIODS_RIGHT = 10
 
-    with open('/tmp/engine_labels.txt', 'r') as f:
+    with open(args.source_labels, 'r') as f:
         labels = list(csv.reader(f, delimiter='\t'))
-    with open('/tmp/motor.wav', 'rb') as f:
+    with open(args.source_wav, 'rb') as f:
         samplerate, data = wavfile.read(f)
     if data.shape[0] == 0:
         return
     time_offset = (data.shape[0] - 1) / samplerate - float(labels[-1][TIME1])
     print(f'Time offset: {time_offset}')
 
+    meta = []
     index = 0
     for key, values_iter in groupby(labels[:-1], key=lambda l: l[LABEL]):
         values = list(values_iter)
@@ -64,35 +67,24 @@ def remix():
             np.iinfo(np.int16).min,
             np.iinfo(np.int16).max).astype(np.int16)
         right = data[begin1:end0]
-        # from IPython import embed
-        # embed()
-        wavfile.write(f'''/tmp/motor_{index:05}_{key.replace('.', '_')}.loop.wav''', samplerate, np.concatenate([left, right]))
+        dest_wav_filename = f'''sample_{index:05}_{key.replace('.', '_')}.loop.wav'''
+        meta.append(dict(
+            filename = dest_wav_filename,
+            key = key,
+            frequency = samplerate / (end0 - begin0 - 1) * (len(values) - 1 - NPERIODS_LEFT - NPERIODS_RIGHT)))
+        wavfile.write(os.path.join(args.out_dir, dest_wav_filename), samplerate, np.concatenate([left, right]))
         # wavfile.write(f'''/tmp/motor_{index:05}_{key.replace('.', '_')}.orig.wav''', samplerate, data[begin0:end1])
         # wavfile.write(f'''/tmp/motor_{index:05}_{key.replace('.', '_')}.a.wav''', samplerate, data[begin0:begin1])
         # wavfile.write(f'''/tmp/motor_{index:05}_{key.replace('.', '_')}.b.wav''', samplerate, data[end0:end1])
         index += 1
+    with open(os.path.join(args.out_dir, 'meta.json'), 'w') as f:
+        json.dump(meta, f, indent=2)
 
-    if False:
-        # f, t, Zxx = stft(data[(2576384-1000):2576384])
-        f, t, Zxx = stft(data, nperseg=20000)
-
-        f0 = np.argmax(np.abs(Zxx) > np.median(np.abs(Zxx), axis=0)[None, :] * 1000, axis=0)
-        # f0 = np.argmax(np.abs(Zxx) > np.percentile(np.abs(Zxx), 98, axis=0)[None, :], axis=0)
-
-        def plot_spectrum():
-            plt.pcolormesh(t, f, np.abs(Zxx), shading='gouraud')
-            plt.title('STFT Magnitude')
-            plt.ylabel('Frequency [Hz]')
-            plt.xlabel('Time [sec]')
-            plt.show()
-
-        def plot_f0():
-            print(Zxx.shape)
-            print(f0.shape)
-            plt.plot(f0)
-            plt.show()
-
-        plot_f0()
 
 if __name__ == '__main__':
-    remix()
+    parser = ArgumentParser()
+    parser.add_argument("--source_labels", required=True)
+    parser.add_argument("--source_wav", required=True)
+    parser.add_argument("--out_dir", required=True)
+
+    remix(parser.parse_args())
