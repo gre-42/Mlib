b121c128c4dafe788317f24ad88241ce7018b663
Marc Kramer <marc.kramer@polyfit42.com>
2023-05-06 20:25:19 +0200

    Simplify AudioDevice

--- Mlib/Audio/Audio_Device.cpp
+++ Mlib/Audio/Audio_Device.cpp
@@ -1,16 +1,12 @@
 #include "Audio_Device.hpp"
-#include <Mlib/Audio/CHK.hpp>
 #include <Mlib/Os/Os.hpp>
 #include <Mlib/Throw_Or_Abort.hpp>
-#include <al.h>
-#include <alc.h>
 #include <stdexcept>
 
 using namespace Mlib;
 
 AudioDevice::AudioDevice() {
     // select the "preferred device"
-    std::scoped_lock mutex{ Mlib::al_error_mutex };
     device_ = alcOpenDevice(nullptr);
     if (device_ == nullptr) {
         THROW_OR_ABORT("Could not open audio device");
@@ -18,7 +14,6 @@ AudioDevice::AudioDevice() {
 }
 
 AudioDevice::~AudioDevice() {
-    std::scoped_lock mutex{ Mlib::al_error_mutex };
     if (!alcCloseDevice(device_)) {
         verbose_abort("Could not close audio device");
     }
