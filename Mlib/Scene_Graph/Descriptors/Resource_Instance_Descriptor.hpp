0bc612106410300968b6be87e4368ead2c785228
Marc Kramer <marc.kramer@polyfit42.com>
2023-05-15 17:03:42 +0200

    Docs

--- Mlib/Scene_Graph/Descriptors/Resource_Instance_Descriptor.hpp
+++ Mlib/Scene_Graph/Descriptors/Resource_Instance_Descriptor.hpp
@@ -8,7 +8,9 @@ namespace Mlib {
 struct ResourceInstanceDescriptor {
     FixedArray<double, 3> position;
     float yangle = 0.f;
-    float scale = 1.f;  // Currently not used
+    float scale = 1.f;  // Currently not used.
+                        // Scaling can be done (per billboard_id) using
+                        // the BillboardAtlasInstance::vertex_scale attribute.
     uint32_t billboard_id;
     template <class Archive>
     void serialize(Archive& archive) {
