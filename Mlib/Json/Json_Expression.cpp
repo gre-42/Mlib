40d169d255e485ab3dc50e7493ab375babee2235
Marc Kramer <marc.kramer@polyfit42.com>
2023-06-06 23:38:54 +0200

    Support sets in eval

--- Mlib/Json/Json_Expression.cpp
+++ Mlib/Json/Json_Expression.cpp
@@ -1,6 +1,7 @@
 #include "Json_Expression.hpp"
 #include <Mlib/Json/Json_View.hpp>
 #include <Mlib/Regex_Select.hpp>
+#include <Mlib/Strings/String.hpp>
 #include <type_traits>
 
 using namespace Mlib;
@@ -24,31 +25,64 @@ static T get(
     }
 }
 
-template <class T>
-T Mlib::eval(
+template <>
+std::string Mlib::eval<std::string>(
     const std::string& expression,
     const JsonView& globals,
     const JsonView& locals)
 {
-    if constexpr (std::is_same_v<T, bool>) {
-        static const DECLARE_REGEX(equality_re, "^(\\w+)=='(\\w+)'$");
-        static const DECLARE_REGEX(inequality_re, "^(\\w+)!='(\\w+)'$");
-        Mlib::re::smatch match;
-        if (Mlib::re::regex_match(expression, match, equality_re)) {
-            return get<std::string>(match[1].str(), globals, locals) == match[2].str();
-        }
-        if (Mlib::re::regex_match(expression, match, inequality_re)) {
-            return get<std::string>(match[1].str(), globals, locals) != match[2].str();
-        }
-        return get<bool>(expression, globals, locals);
-    } else {
-        static_assert(FalseJsonEval<T>::value, "Unsupported type in Mlib::eval");
+    static const DECLARE_REGEX(string_literal_re, "'(\\w*)'");
+    Mlib::re::smatch match;
+    if (Mlib::re::regex_match(expression, match, string_literal_re)) {
+        return match[1].str();
     }
+    return get<std::string>(expression, globals, locals);
+}
+
+template <>
+std::set<std::string> Mlib::eval<std::set<std::string>>(
+    const std::string& expression,
+    const JsonView& globals,
+    const JsonView& locals)
+{
+    static const DECLARE_REGEX(in_re, "^\\{(.*)\\}$");
+    static const DECLARE_REGEX(comma_re, ", ");
+    Mlib::re::smatch match;
+    std::set<std::string> result;
+    if (!Mlib::re::regex_match(expression, match, in_re)) {
+        THROW_OR_ABORT("Could not parse as set: \"" + expression + '"');
+    }
+    for (const auto& element : string_to_list(match[1].str(), comma_re)) {
+        if (!result.insert(eval<std::string>(element, globals, locals)).second) {
+            THROW_OR_ABORT("Duplicate element: \"" + element + '"');
+        }
+    }
+    return result;
+}
+
+template <>
+bool Mlib::eval<bool>(
+    const std::string& expression,
+    const JsonView& globals,
+    const JsonView& locals)
+{
+    static const DECLARE_REGEX(in_re, "^(\\w+) in (.*)$");
+    static const DECLARE_REGEX(not_in_re, "^(\\w+) not in (.*)$");
+    Mlib::re::smatch match;
+    if (Mlib::re::regex_match(expression, match, in_re)) {
+        auto elems = eval<std::set<std::string>>(match[2].str(), globals, locals);
+        return elems.contains(eval<std::string>(match[1].str(), globals, locals));
+    }
+    if (Mlib::re::regex_match(expression, match, not_in_re)) {
+        auto elems = eval<std::set<std::string>>(match[2].str(), globals, locals);
+        return !elems.contains(eval<std::string>(match[1].str(), globals, locals));
+    }
+    return get<bool>(expression, globals, locals);
 }
 
 template <class T>
 T Mlib::eval(const std::string& expression, const JsonView& variables) {
-    return eval<T>(expression, variables, JsonView{{}});
+    return eval<T>(expression, variables, JsonView{std::map<std::string, std::string>{}});
 }
 
 template bool Mlib::eval<bool>(const std::string& expression, const JsonView& variables);
