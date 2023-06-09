210d259af098b9df45a135dd0f590cbfdd420d76
Marc Kramer <marc.kramer@polyfit42.com>
2023-06-09 20:38:07 +0200

    Fix number strings

--- Mlib/Scene/Load_Scene_Functions/Resources/Load_Osm_Resource.cpp
+++ Mlib/Scene/Load_Scene_Functions/Resources/Load_Osm_Resource.cpp
@@ -22,7 +22,7 @@
 #include <Mlib/Throw_Or_Abort.hpp>
 #include <filesystem>
 
-static uint32_t CACHE_FILE_VERSION = 16;
+static uint32_t CACHE_FILE_VERSION = 17;
 
 namespace fs = std::filesystem;
 
