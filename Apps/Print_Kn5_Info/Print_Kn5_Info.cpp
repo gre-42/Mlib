a8b8065859da9995dca5aeb913d4cb56b05263da
Marc Kramer <marc.kramer@polyfit42.com>
2023-07-16 17:20:53 +0200

    Add "print_kn5_info"

---
+++ Apps/Print_Kn5_Info/Print_Kn5_Info.cpp
@@ -0,0 +1,15 @@
+#include <Mlib/Arg_Parser.hpp>
+#include <Mlib/Geometry/Mesh/Load_Kn5.hpp>
+
+using namespace Mlib;
+
+int main(int argc, char **argv) {
+    const ArgParser parser(
+        "Usage: print_kn5_info <file.kn5>",
+        {},
+        {});
+    const auto args = parser.parsed(argc, argv);
+    args.assert_num_unnamed(1);
+    load_kn5(args.unnamed_value(0), true);
+    return 0;
+}
