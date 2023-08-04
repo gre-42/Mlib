80932fabbf801b9c5b3e49d3ddc0b6979f711428
Marc Kramer <marc.kramer@polyfit42.com>
2023-08-04 22:44:08 +0200

    Fix sun path

--- Tests/Geography/Geography_Test.cpp
+++ Tests/Geography/Geography_Test.cpp
@@ -8,7 +8,7 @@ using namespace Mlib;
 void test_sun_position() {
     using namespace std::chrono;
     constexpr system_clock::time_point time = sys_days{February/4/2042};
-    StbImage1 im1{ArrayShape{512, 1024}};
+    StbImage1 im1{ArrayShape{256, 512}};
     for (size_t ilat = 0; ilat < im1.shape(0); ++ilat) {
         for (size_t ilon = 0; ilon < im1.shape(1); ++ilon) {
             auto lat = double(ilat) * M_PI / double(im1.shape(0)) - M_PI / 2.;
@@ -17,7 +17,7 @@ void test_sun_position() {
             im1(ilat, ilon) = (uint8_t)std::clamp(std::round(n(2) * 255.), 0., 255.);
         }
     }
-    im1.save_to_file("/tmp/sun.png");
+    im1.save_to_file("TestOut/sun.png");
 }
 
 int main(int argc, const char** argv) {
