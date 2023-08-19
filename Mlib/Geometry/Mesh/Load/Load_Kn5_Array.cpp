9ab170fba80a03ce01ab46e3353c2d462156416a
Marc Kramer <marc.kramer@polyfit42.com>
2023-08-19 12:19:09 +0200

    Change blend-mode for ksPerPixelAT_NM

--- Mlib/Geometry/Mesh/Load/Load_Kn5_Array.cpp
+++ Mlib/Geometry/Mesh/Load/Load_Kn5_Array.cpp
@@ -258,11 +258,12 @@ std::list<std::shared_ptr<ColoredVertexArray<TPos>>> Mlib::load_kn5_array(
                         tl.material.occluded_pass = ExternalRenderPassType::LIGHTMAP_BLACK_NODE;
                         tl.material.occluder_pass = ExternalRenderPassType::NONE;
                     }
-                    if (material.shader == "ksPerPixel") {                 // required for Akina track
+                    if ((material.shader == "ksPerPixel") ||                // required for Akina track
+                        (material.shader == "ksPerPixelAT_NM"))             // required for Semetin track
+                    {
                         tl.material.blend_mode = BlendMode::BINARY_05;
                     } else if ((material.shader == "ksPerPixelAlpha") ||
-                               (material.shader == "ksPerPixelAT") ||      // required for Hondarribia track
-                               (material.shader == "ksPerPixelAT_NM") ||
+                               (material.shader == "ksPerPixelAT") ||       // required for Hondarribia track
                                (material.shader == "ksPerPixelMultiMap_AT_NMDetail"))
                     {
                         tl.material.blend_mode = cfg.blend_mode;
