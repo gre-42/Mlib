4520d52afcb06565e27f23f228d2fa475dfb8a68
Marc Kramer <marc.kramer@polyfit42.com>
2023-09-03 16:42:45 +0200

    UppeLeftArray->LeftArray

--- Mlib/Math/Transformation/Translation_Matrix.hpp
+++ Mlib/Math/Transformation/Translation_Matrix.hpp
@@ -20,15 +20,15 @@ private:
 };
 
 template <class TData, size_t n>
-class UpperLeftArray: public BaseDenseFixedArray<UpperLeftArray<TData, n>, TData, n, n> {
+class LeftArray: public BaseDenseFixedArray<LeftArray<TData, n>, TData, n+1, n> {
 public:
-    UpperLeftArray(const FixedArray<TData, n+1, n+1>& a)
+    LeftArray(const FixedArray<TData, n+1, n+1>& a)
     : a_{a}
     {}
-    constexpr static FixedArrayShape<n, n> shape() {
-        return FixedArrayShape<n, n>();
+    constexpr static FixedArrayShape<n+1, n> shape() {
+        return FixedArrayShape<n+1, n>();
     }
-    const UpperLeftArray& rows_as_1D() const {
+    const LeftArray& rows_as_1D() const {
         return *this;
     }
     const TData& operator () (size_t r, size_t c) const {
@@ -41,8 +41,8 @@ private:
 template <class T, size_t n>
 FixedArray<T, n+1, n+1> operator * (const FixedArray<T, n+1, n+1>& a, const TranslationMatrix<T, n>& b) {
     auto result = a;
-    auto Rb = dot1d(UpperLeftArray<T, n>{a}, b.t());
-    for (size_t i = 0; i < n; ++i) {
+    auto Rb = dot1d(LeftArray<T, n>{a}, b.t());
+    for (size_t i = 0; i <= n; ++i) {
         result(i, n) += Rb(i);
     }
     return result;
