dfaf68e0a175d70f21c5ccf5e06bba442be5d0fd
Marc Kramer <marc.kramer@polyfit42.com>
2023-09-10 12:04:56 +0200

    Increase file version

--- Mlib/Scene/Load_Scene_Functions/Resources/Load_Osm_Resource.cpp
+++ Mlib/Scene/Load_Scene_Functions/Resources/Load_Osm_Resource.cpp
@@ -22,7 +22,7 @@
 #include <Mlib/Throw_Or_Abort.hpp>
 #include <filesystem>
 
-static uint32_t CACHE_FILE_VERSION = 23;
+static uint32_t CACHE_FILE_VERSION = 24;
 
 namespace fs = std::filesystem;
 
