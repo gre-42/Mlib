c2f3a34c3addc4b6a7cbc7ca7a3e9df931c5937a
Marc Kramer <marc.kramer@mymail.com>
2023-09-20 19:18:19 +0200

    Deploy ALUT release DLL

--- package_scene_renderer.cmd
+++ package_scene_renderer.cmd
@@ -3,7 +3,7 @@ cd /D "%~dp0" || exit /b
 IF "%CMAKE_BUILD_TYPE%"=="" set CMAKE_BUILD_TYPE=Release
 
 for %%I in ( ^
-    freealut\build\src\%CMAKE_BUILD_TYPE%\alut.dll ^
+    freealut\build\src\Release\alut.dll ^
     VSRecastBuild\Detour\%CMAKE_BUILD_TYPE%\Detour.dll ^
     VSRecastBuild\DebugUtils\%CMAKE_BUILD_TYPE%\DebugUtils.dll ^
     VSRecastBuild\Recast\%CMAKE_BUILD_TYPE%\Recast.dll ^
