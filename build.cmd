acefa5192b25e5222182246a7a57376a89096fdb
Marc Kramer <marc.kramer@mymail.com>
2023-09-22 19:54:17 +0200

    Switch to openal-soft

--- build.cmd
+++ build.cmd
@@ -4,7 +4,7 @@ IF "%CMAKE_BUILD_TYPE%"=="" set CMAKE_BUILD_TYPE=Release
 set BUILD_DIR=%BUILD_PREFIX%VS
 mkdir %BUILD_DIR%
 cd %BUILD_DIR% || exit /b
-set OPENALDIR=C:\Program Files (x86)\OpenAL 1.1 SDK
+REM set OPENALDIR=C:\Program Files (x86)\OpenAL 1.1 SDK
 cmake .. %CMAKE_OPTIONS% || exit /b
 cmake ^
     --build . ^
--- package_scene_renderer.cmd
+++ package_scene_renderer.cmd
@@ -3,6 +3,7 @@ cd /D "%~dp0" || exit /b
 IF "%CMAKE_BUILD_TYPE%"=="" set CMAKE_BUILD_TYPE=Release
 
 for %%I in ( ^
+    openal-soft\build\Release\OpenAL32.dll ^
     freealut\build\src\Release\alut.dll ^
     VSRecastBuild\Detour\%CMAKE_BUILD_TYPE%\Detour.dll ^
     VSRecastBuild\DebugUtils\%CMAKE_BUILD_TYPE%\DebugUtils.dll ^
