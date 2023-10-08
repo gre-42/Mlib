2d2b5faa3fc3ab9308143127ebf884dd26ab8234
Marc Kramer <marc.kramer@mymail.com>
2023-10-08 20:40:33 +0200

    Portable FastUniformIntRandomNumberGenerator

--- Mlib/Stats/Fast_Random_Number_Generators.hpp
+++ Mlib/Stats/Fast_Random_Number_Generators.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <Mlib/Math/Float_Type.hpp>
+#include <Mlib/Memory/Integral_Cast.hpp>
 #include <random>
 #include <tuple>
 
@@ -27,25 +28,48 @@ private:
     std::uniform_real_distribution<typename FloatType<TData>::value_type> d_;
 };
 
+// The implementation differs among compilers (e.g. g++ vs. MSVC)
+// https://stackoverflow.com/questions/44520973/how-can-i-get-an-implementation-agnostic-version-of-stduniform-int-distributio
+// template <class TData>
+// class FastUniformIntRandomNumberGenerator {
+// public:
+//     explicit FastUniformIntRandomNumberGenerator(unsigned int seed, const TData& low, const TData& high)
+//     : e_(seed),
+//       d_(low, high)
+//     {
+// 		std::ignore = e_();
+//     }
+//     TData operator () () {
+//         return d_(e_);
+//     }
+//     void seed(unsigned int seed) {
+//         e_.seed(seed);
+//         std::ignore = e_();
+//     }
+// private:
+//     std::minstd_rand e_;
+//     std::uniform_int_distribution<TData> d_;
+// };
+
 template <class TData>
 class FastUniformIntRandomNumberGenerator {
 public:
     explicit FastUniformIntRandomNumberGenerator(unsigned int seed, const TData& low, const TData& high)
     : e_(seed),
-      d_(low, high)
+      d_(integral_cast<int>(low), integral_cast<int>(high + 1))
     {
-		std::ignore = e_();
+        std::ignore = e_();
     }
     TData operator () () {
-        return d_(e_);
+        return (TData)std::floor(d_(e_));
     }
     void seed(unsigned int seed) {
         e_.seed(seed);
-		std::ignore = e_();
+        std::ignore = e_();
     }
 private:
     std::minstd_rand e_;
-    std::uniform_int_distribution<TData> d_;
+    std::uniform_real_distribution<double> d_;
 };
 
 template <class TData>
