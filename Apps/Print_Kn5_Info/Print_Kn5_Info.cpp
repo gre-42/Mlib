bd4883ed2b28751e0f27f7b786e032514d295d00
Marc Kramer <marc.kramer@mymail.com>
2023-10-28 13:21:12 +0200

    Export textures

--- Apps/Print_Kn5_Info/Print_Kn5_Info.cpp
+++ Apps/Print_Kn5_Info/Print_Kn5_Info.cpp
@@ -1,19 +1,39 @@
 #include <Mlib/Arg_Parser.hpp>
 #include <Mlib/Geometry/Mesh/Load/Load_Kn5.hpp>
 #include <Mlib/Os/Os.hpp>
+#include <Mlib/Regex/Regex_Select.hpp>
+#include <filesystem>
 
 using namespace Mlib;
 
 int main(int argc, char **argv) {
     const ArgParser parser(
-        "Usage: print_kn5_info <file.kn5>",
+        "Usage: print_kn5_info <file.kn5> [--export <pattern>]",
         {},
-        {});
+        {"--export"});
     try {
         const auto args = parser.parsed(argc, argv);
         args.assert_num_unnamed(1);
-        load_kn5(args.unnamed_value(0), true);
-    } catch (const std::runtime_error& e) {
+        auto kn5 = load_kn5(args.unnamed_value(0), true);
+        if (args.has_named_value("--export")) {
+            DECLARE_REGEX(re, args.named_value("--export"));
+            for (const auto& [name, data] : kn5.textures) {
+                if (!Mlib::re::regex_search(name, re)) {
+                    continue;
+                }
+                auto tex_filename = std::filesystem::path{ "textures" } / name;
+                auto f = create_ofstream(tex_filename, std::ios::binary);
+                if (f->fail()) {
+                    THROW_OR_ABORT("Could not open file for write: \"" + tex_filename.string() + '"');
+                }
+                f->write((const char*)data.data.data(), data.data.size());
+                f->flush();
+                if (f->fail()) {
+                    THROW_OR_ABORT("Could not write to file: \"" + tex_filename.string() + '"');
+                }
+            }
+        }
+    } catch (const std::exception& e) {
         lerr() << e.what();
         return 1;
     }
