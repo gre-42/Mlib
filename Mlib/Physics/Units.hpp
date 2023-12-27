956e7d6abcc05c5d3cd0f79249c2760853e6c6f8
Marc Kramer <marc.kramer@mymail.com>
2023-12-27 23:10:51 +0100

    cm

--- Mlib/Physics/Units.hpp
+++ Mlib/Physics/Units.hpp
@@ -3,6 +3,10 @@
 
 namespace Mlib {
 
+static const float milli = 1.f / 1000.f;
+static const float centi = 1.f / 100.f;
+static const float kilo = 1000.f;
+	
 static const float s = float{1e3};
 static const float meters = 1.f;
 static const float kg = float(1e-3);
@@ -13,8 +17,11 @@ static const float degrees = float(M_PI / 180);
 static const float minutes = 60.f * s;
 static const float hours = 60.f * minutes;
 
-static const float kilo = 1000.f;
-static const float kph = kilo * meters / hours;
+static const float mm = milli * meters;
+static const float cm = centi * meters;
+static const float km = kilo * meters;
+
+static const float kph = km / hours;
 static const float N = kg * meters / (s * s);
 static const float W = N * meters / s;
 static const float hp = 735.5f * W;
