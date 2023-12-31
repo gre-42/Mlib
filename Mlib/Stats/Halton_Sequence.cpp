ef315662905dc494ec7a89cd6d4abcc03051da06
Marc Kramer <marc.kramer@mymail.com>
2024-01-01 00:35:31 +0100

    Circumvent g++ warning

--- Mlib/Stats/Halton_Sequence.cpp
+++ Mlib/Stats/Halton_Sequence.cpp
@@ -14,8 +14,8 @@ void Mlib::generate_halton_lut(size_t nnumbers, size_t block_size) {
 	std::mt19937 g(0);
 	for (size_t i = 0; i < random_numbers.size(); i += block_size) {
 		std::shuffle(
-			random_numbers.begin() + i,
-			random_numbers.begin() + std::min(random_numbers.size(), i + block_size),
+			random_numbers.data() + i,
+			random_numbers.data() + std::min(random_numbers.size(), i + block_size),
 			g);
 	}
 	{
