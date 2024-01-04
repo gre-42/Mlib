ad38de4a7be3d766d4e4723774d0d40d7ec1c33d
Marc Kramer <marc.kramer@mymail.com>
2024-01-04 18:05:33 +0100

    Update material colors

--- Mlib/Osm_Loader/Osm_Map_Resource/Material_Colors.hpp
+++ Mlib/Osm_Loader/Osm_Map_Resource/Material_Colors.hpp
@@ -3,24 +3,22 @@
 
 namespace Mlib {
 
-static const OrderableFixedArray<float, 3> o3(float f) {
-    return OrderableFixedArray<float, 3>(f);
-}
+// See the "plot_fresnel.py" script for tuning these parameters.
 
-static const Shading TARMAC_REFLECTANCE{ .specularity = 0.5f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.15f, .exponent = 4.f}, .ambience = 1.f} };
-static const Shading GRAVEL_REFLECTANCE{ .specularity = 0.f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.15f, .exponent = 4.f}, .ambience = 1.f} };
-static const Shading SNOW_REFLECTANCE{ .specularity = 0.4f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.3f, .exponent = 4.f}, .ambience = 1.f} };
-static const Shading ICE_REFLECTANCE{ .specularity = 0.5f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.3f, .exponent = 4.f}, .ambience = 1.f} };
-static const Shading SAND_REFLECTANCE{ .specularity = 0.1f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.15f, .exponent = 4.f}, .ambience = 1.f} };
-static const Shading GRASS_REFLECTANCE{ .specularity = 0.f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.15f, .exponent = 4.f}, .ambience = 1.f} };
-static const Shading DIRT_REFLECTANCE{ .specularity = 0.2f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.3f, .exponent = 4.f}, .ambience = 1.f} };
-static const Shading STONE_REFLECTANCE{ .specularity = 0.f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.15f, .exponent = 4.f}, .ambience = 1.f} };
+static const Shading TARMAC_REFLECTANCE{ .specularity = 0.5f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.3f, .exponent = 4.f}, .ambience = 1.f} };
+static const Shading GRAVEL_REFLECTANCE{ .specularity = 0.f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.5f, .exponent = 5.f}, .ambience = 1.f} };
+static const Shading SNOW_REFLECTANCE{ .specularity = 0.f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.3f, .exponent = 5.f}, .ambience = 1.f} };
+static const Shading ICE_REFLECTANCE{ .specularity = 0.f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.3f, .exponent = 5.f}, .ambience = 1.f} };
+static const Shading SAND_REFLECTANCE{ .specularity = 0.f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.15f, .exponent = 5.f}, .ambience = 1.f} };
+static const Shading GRASS_REFLECTANCE{ .specularity = 0.f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.1f, .exponent = 5.f}, .ambience = 1.f} };
+static const Shading DIRT_REFLECTANCE{ .specularity = 0.f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.3f, .exponent = 5.f}, .ambience = 1.f} };
+static const Shading STONE_REFLECTANCE{ .specularity = 0.f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.15f, .exponent = 5.f}, .ambience = 1.f} };
 
-static const Shading CURB_REFLECTANCE{ .specularity = 0.5f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.15f, .exponent = 4.f}, .ambience = 1.f} };
-static const Shading MUD_REFLECTANCE{ .specularity = 0.5f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.15f, .exponent = 4.f}, .ambience = 1.f} };
+static const Shading CURB_REFLECTANCE{ .specularity = 0.5f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.15f, .exponent = 5.f}, .ambience = 1.f} };
+static const Shading MUD_REFLECTANCE{ .specularity = 0.5f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.15f, .exponent = 5.f}, .ambience = 1.f} };
 
-static const Shading ROOF_REFLECTANCE{ .specularity = 0.2f, .fresnel = {.reflectance = {.min = 0.3f, .max = 0.7f, .exponent = 4.f}, .ambience = 1.f} };
-static const Shading CEILING_REFLECTANCE{ .specularity = 0.2f, .fresnel = {.reflectance = {.min = 0.3f, .max = 0.7f, .exponent = 4.f}, .ambience = 1.f} };
-static const Shading DEFAULT_REFLECTANCE{ .fresnel = {.reflectance = {.min = 0.3f, .max = 0.7f, .exponent = 4.f}, .ambience = 1.f} };
+static const Shading ROOF_REFLECTANCE{ .specularity = 0.2f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.7f, .exponent = 5.f}, .ambience = 1.f} };
+static const Shading CEILING_REFLECTANCE{ .specularity = 0.2f, .fresnel = {.reflectance = {.min = 0.f, .max = 0.15f, .exponent = 5.f}, .ambience = 1.f} };
+static const Shading DEFAULT_REFLECTANCE{ .fresnel = {.reflectance = {.min = 0.f, .max = 0.15f, .exponent = 5.f}, .ambience = 1.f} };
 
 }
