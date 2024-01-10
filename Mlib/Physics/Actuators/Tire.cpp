1809de6d3559762d49986528171fc5261c28f5af
Marc Kramer <marc.kramer@mymail.com>
2024-01-10 20:10:09 +0100

    PEP8

--- Mlib/Physics/Actuators/Tire.cpp
+++ Mlib/Physics/Actuators/Tire.cpp
@@ -12,18 +12,18 @@ Tire::Tire(
     const CombinedMagicFormula<float>& magic_formula,
     const FixedArray<float, 3>& position,
     float radius)
-: BaseRotor{ engine, delta_engine },
-  magic_formula{magic_formula},
-  shock_absorber_position{0},
-  angle_x{0},
-  angle_y{0},
-  // accel_x{0},
-  brake_force{brake_force},
-  sKs{sKs},
-  sKa{sKa},
-  stiction_coefficient{stiction_coefficient},
-  position{position},
-  radius{radius}
+    : BaseRotor{ engine, delta_engine }
+    , magic_formula{ magic_formula }
+    , shock_absorber_position{ 0 }
+    , angle_x{ 0 }
+    , angle_y_{ 0 }
+    // accel_x{0}
+    , brake_force{ brake_force }
+    , sKs{ sKs }
+    , sKa{ sKa }
+    , stiction_coefficient{ stiction_coefficient }
+    , position{ position }
+    , radius{ radius }
 {}
 
 void Tire::advance_time(float dt) {
