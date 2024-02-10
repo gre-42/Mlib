627c4ae8c6810734af6460658fdb6a0f63747a3d
Marc Kramer <marc.kramer@mymail.com>
2024-02-10 15:52:13 +0100

    Simplify physics_material_to_string

--- Mlib/Geometry/Physics_Material.cpp
+++ Mlib/Geometry/Physics_Material.cpp
@@ -56,24 +56,20 @@ PhysicsMaterial Mlib::physics_material_from_string(const std::string& s) {
 }
 
 std::string Mlib::physics_material_to_string(PhysicsMaterial p) {
-    static const std::map<PhysicsMaterial, std::string> m{
-        {PhysicsMaterial::NONE, "none"},
-        {PhysicsMaterial::SURFACE_BASE_TARMAC, "surface_base_tarmac"},
-        {PhysicsMaterial::SURFACE_BASE_GRAVEL, "surface_base_gravel"},
-        {PhysicsMaterial::SURFACE_BASE_SNOW, "surface_base_snow"},
-        {PhysicsMaterial::SURFACE_BASE_ICE, "surface_base_ice"},
-        {PhysicsMaterial::SURFACE_BASE_SAND, "surface_base_sand"},
-        {PhysicsMaterial::SURFACE_BASE_GRASS, "surface_base_grass"},
-        {PhysicsMaterial::SURFACE_BASE_DIRT, "surface_base_dirt"},
-        {PhysicsMaterial::SURFACE_BASE_TIRE, "surface_base_tire"},
-        {PhysicsMaterial::SURFACE_BASE_STONE, "surface_base_stone"},
-        {PhysicsMaterial::SURFACE_BASE_FOLIAGE, "surface_base_foliage"}
+    switch (p) {
+        case PhysicsMaterial::NONE: return "none";
+        case PhysicsMaterial::SURFACE_BASE_TARMAC: return "surface_base_tarmac";
+        case PhysicsMaterial::SURFACE_BASE_GRAVEL: return "surface_base_gravel";
+        case PhysicsMaterial::SURFACE_BASE_SNOW: return "surface_base_snow";
+        case PhysicsMaterial::SURFACE_BASE_ICE: return "surface_base_ice";
+        case PhysicsMaterial::SURFACE_BASE_SAND: return "surface_base_sand";
+        case PhysicsMaterial::SURFACE_BASE_GRASS: return "surface_base_grass";
+        case PhysicsMaterial::SURFACE_BASE_DIRT: return "surface_base_dirt";
+        case PhysicsMaterial::SURFACE_BASE_TIRE: return "surface_base_tire";
+        case PhysicsMaterial::SURFACE_BASE_STONE: return "surface_base_stone";
+        case PhysicsMaterial::SURFACE_BASE_FOLIAGE: return "surface_base_foliage";
+        default: return "PhysicsMaterial(" + std::to_string((uint32_t)p) + ')';
     };
-    auto it = m.find(p);
-    if (it == m.end()) {
-        return "PhysicsMaterial(" + std::to_string((uint32_t)p) + ')';
-    }
-    return it->second;
 }
 
 void Mlib::from_json(const nlohmann::json& j, PhysicsMaterial& p) {
