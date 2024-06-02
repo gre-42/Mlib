7c1fd2f1821003c1d9369e0c779dea9f6698e1cb
Marc Kramer <marc.kramer@mymail.com>
2024-06-02 15:09:22 +0200

    Ignore uninitialized variable

--- Mlib/Uninitialized.hpp
+++ Mlib/Uninitialized.hpp
@@ -4,6 +4,9 @@ namespace Mlib {
 
 struct Uninitialized {};
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-variable"
 static Uninitialized uninitialized;
+#pragma GCC diagnostic pop
 
 }
