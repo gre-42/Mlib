c76f01ea12816a40ed98ce00424ef724eb950cb8
Marc Kramer <marc.kramer@mymail.com>
2024-09-10 23:56:14 +0200

    Update cache file version

--- Mlib/Scene/Load_Scene_Functions/Resources/Load_Osm_Resource.cpp
+++ Mlib/Scene/Load_Scene_Functions/Resources/Load_Osm_Resource.cpp
@@ -23,7 +23,7 @@
 #include <Mlib/Throw_Or_Abort.hpp>
 #include <filesystem>
 
-static uint32_t CACHE_FILE_VERSION = 49;
+static uint32_t CACHE_FILE_VERSION = 50;
 
 namespace fs = std::filesystem;
 
