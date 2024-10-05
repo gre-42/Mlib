cca4bc013ae982c89c2a1d41df8fd9a6532d3604
Marc Kramer <marc.kramer@mymail.com>
2024-10-05 13:39:58 +0200

    Fix sigmoid

--- Mlib/Math/Sigmoid.hpp
+++ Mlib/Math/Sigmoid.hpp
@@ -5,43 +5,35 @@
 namespace Mlib {
 
 // From: https://math.stackexchange.com/a/3253471/233679
-template <class T>
-static T sigmoid(T t)
+// See also the comment from Dr. Timofey Prodanov,
+// Dec 21, 2020 at 10:51
+template <class X>
+static X sigmoid(X x)
 {
-    auto c1 = scalar_type_t<T>(1);
-    auto a = c1 / squared(c1 / t - c1);
-    return c1 - c1 / (c1 + a);
+    auto c1 = scalar_type_t<X>(1);
+    auto a = squared(c1 / x - c1);
+    return c1 / (c1 + a);
 }
 
 // From: https://math.stackexchange.com/a/3253471/233679
 // See also the comment from Dr. Timofey Prodanov,
 // Dec 21, 2020 at 10:51
-template <class T>
-static T inv_sigmoid(T t)
+template <class X, class K>
+static X sigmoid(const X& x, const K& k)
 {
-    auto c1 = scalar_type_t<T>(1);
-    auto a = squared(c1 / t - c1);
+    auto c1 = scalar_type_t<X>(1);
+    auto a = pow(c1 / x - c1, k);
     return c1 / (c1 + a);
 }
 
-// From: https://math.stackexchange.com/a/3253471/233679
-template <class T, class TK>
-static T sigmoid(const T& t, const TK& k)
+// From: https://math.stackexchange.com/a/4798714/233679
+template <class X, class T, class K>
+static X sigmoid(const X& x, const T& t, const K& k)
 {
-    auto c1 = scalar_type_t<T>(1);
-    auto a = c1 / pow(c1 / t - c1, k);
-    return c1 - c1 / (c1 + a);
-}
-
-// From: https://math.stackexchange.com/a/3253471/233679
-// See also the comment from Dr. Timofey Prodanov,
-// Dec 21, 2020 at 10:51
-template <class T, class TK>
-static T inv_sigmoid(const T& t, const TK& k)
-{
-    auto c1 = scalar_type_t<T>(1);
-    auto a = pow(c1 / t - c1, k);
+    auto c1 = scalar_type_t<X>(1);
+    auto a = pow(pow(x, std::log((T)2) / std::log(t)) - c1, k);
     return c1 / (c1 + a);
 }
+
 
 }
