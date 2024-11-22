6b24bce9ba1d790a8794eb8b7c6c264423b4b80d
Marc Kramer <marc.kramer@mymail.com>
2024-11-22 20:49:34 +0100

    Increase OSM cache file version

--- Mlib/Scene/Load_Scene_Functions/Resources/Load_Osm_Resource.cpp
+++ Mlib/Scene/Load_Scene_Functions/Resources/Load_Osm_Resource.cpp
@@ -23,7 +23,7 @@
 #include <Mlib/Throw_Or_Abort.hpp>
 #include <filesystem>
 
-static uint32_t CACHE_FILE_VERSION = 54;
+static uint32_t CACHE_FILE_VERSION = 55;
 
 namespace fs = std::filesystem;
 
