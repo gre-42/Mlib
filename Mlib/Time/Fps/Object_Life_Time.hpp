3307eba0c083627db0e15f2d016c87e2bb5e8c99
Marc Kramer <marc.kramer@mymail.com>
2025-01-11 22:17:27 +0100

    Add ObjectLifeTime

---
+++ Mlib/Time/Fps/Object_Life_Time.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <chrono>
+#include <functional>
+
+namespace Mlib {
+
+class ObjectLifeTime {
+public:
+	explicit ObjectLifeTime(std::function<void(std::chrono::steady_clock::duration elapsed)> func);
+	~ObjectLifeTime();
+private:
+	std::function<void(std::chrono::steady_clock::duration elapsed)> func_;
+	std::chrono::steady_clock::time_point start_time_;
+};
+
+}
