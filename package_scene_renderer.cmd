9d92072c8fc88eaa694fce9dbb81bbef0bdac46f
Marc Kramer <marc.kramer@mymail.com>
2025-01-25 23:36:08 +0100

    PEP8

--- package_scene_renderer.cmd
+++ package_scene_renderer.cmd
@@ -5,8 +5,8 @@ IF "%CMAKE_BUILD_TYPE%"=="" set CMAKE_BUILD_TYPE=Release
 for %%I in ( ^
     openal-soft\build\Release\OpenAL32.dll ^
     freealut\build\src\Release\alut.dll ^
-    "C:\Program Files/OpenSSL-Win64/libssl-3-x64.dll" ^
-    "C:\Program Files/OpenSSL-Win64/libcrypto-3-x64.dll" ^
+    "C:\Program Files\OpenSSL-Win64\libssl-3-x64.dll" ^
+    "C:\Program Files\OpenSSL-Win64\libcrypto-3-x64.dll" ^
     VSRecastBuild\Detour\%CMAKE_BUILD_TYPE%\Detour.dll ^
     VSRecastBuild\DebugUtils\%CMAKE_BUILD_TYPE%\DebugUtils.dll ^
     VSRecastBuild\Recast\%CMAKE_BUILD_TYPE%\Recast.dll ^
