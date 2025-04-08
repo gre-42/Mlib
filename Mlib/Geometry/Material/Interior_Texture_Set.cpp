a647627f44b152cb228bdbde7374a580265e6a64
Marc Kramer <gre@die-optimisten.net>
2025-04-08 17:24:27 +0200

    Fix Mlib::index

--- Mlib/Geometry/Material/Interior_Texture_Set.cpp
+++ Mlib/Geometry/Material/Interior_Texture_Set.cpp
@@ -7,12 +7,17 @@ size_t Mlib::size(InteriorTextureSet a) {
     if (!any(a)) {
         return 0;
     }
-    a >>= 1;
-    for (size_t result = 5; ; ++result) {
+    if (!any(a & InteriorTextureSet::INTERIOR_COLORS)) {
+        THROW_OR_ABORT("Interior textures have not colors");
+    }
+    for (size_t result = 5;;) {
+        a >>= 1;
         if (!any(a)) {
             return result;
         }
-        a >>= 1;
+        if (any(a & (InteriorTextureSet)1)) {
+            ++result;
+        }
     }
 }
 
@@ -20,14 +25,26 @@ size_t Mlib::index(InteriorTextureSet available, InteriorTextureSet x) {
     if (!any(available & InteriorTextureSet::INTERIOR_COLORS)) {
         THROW_OR_ABORT("Interior colors not set");
     }
-    for (size_t result = 5; any(available);) {
-        if (any(available & x)) {
+    if (!any(available & x)) {
+        THROW_OR_ABORT("Interior texture not found");
+    }
+    if (!any(available & InteriorTextureSet::INTERIOR_COLORS)) {
+        THROW_OR_ABORT("Interior textures have not colors");
+    }
+    available >>= 1;
+    x >>= 1;
+    for (size_t result = 4;;) {
+        if (!any(x)) {
+            if (result < 5) {
+                THROW_OR_ABORT("Interior texture index too small");
+            }
             return result;
         }
         if (any(available & (InteriorTextureSet)1)) {
             ++result;
         }
         available >>= 1;
+        x >>= 1;
     }
     THROW_OR_ABORT("Could not find interior texture index");
 }
