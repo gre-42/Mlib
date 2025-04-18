0bd005a16bfa308561c27948f83266613e87abec
Marc Kramer <gre@die-optimisten.net>
2025-04-18 17:40:35 +0200

    Fix CLang warnings

--- Mlib/Geometry/Intersection/Intersectors/Swept_Sphere_Aabb.hpp
+++ Mlib/Geometry/Intersection/Intersectors/Swept_Sphere_Aabb.hpp
@@ -11,40 +11,40 @@ public:
         const FixedArray<CompressedScenePos, 3>& min,
         const FixedArray<CompressedScenePos, 3>& max,
         const CompressedScenePos& radius);
-    virtual BoundingSphere<CompressedScenePos, 3> bounding_sphere() const;
-    virtual AxisAlignedBoundingBox<CompressedScenePos, 3> aabb() const;
+    virtual BoundingSphere<CompressedScenePos, 3> bounding_sphere() const override;
+    virtual AxisAlignedBoundingBox<CompressedScenePos, 3> aabb() const override;
     virtual bool touches(
         const CollisionPolygonSphere<CompressedScenePos, 4>& q,
         ScenePos& overlap,
         FixedArray<ScenePos, 3>& intersection_point,
-        FixedArray<SceneDir, 3>& normal) const;
+        FixedArray<SceneDir, 3>& normal) const override;
     virtual bool touches(
         const CollisionPolygonSphere<CompressedScenePos, 3>& t,
         ScenePos& overlap,
         FixedArray<ScenePos, 3>& intersection_point,
-        FixedArray<SceneDir, 3>& normal) const;
+        FixedArray<SceneDir, 3>& normal) const override;
     virtual bool touches(
         const CollisionRidgeSphere<CompressedScenePos>& r1,
         ScenePos& overlap,
         FixedArray<ScenePos, 3>& intersection_point,
-        FixedArray<SceneDir, 3>& normal) const;
+        FixedArray<SceneDir, 3>& normal) const override;
     virtual bool touches(
         const CollisionLineSphere<CompressedScenePos>& l1,
         ScenePos& overlap,
         ScenePos& ray_t,
         FixedArray<ScenePos, 3>& intersection_point,
-        FixedArray<SceneDir, 3>& normal) const;
+        FixedArray<SceneDir, 3>& normal) const override;
     virtual bool touches(
         const IIntersectable& intersectable,
         ScenePos& overlap,
         FixedArray<ScenePos, 3>& intersection_point,
-        FixedArray<SceneDir, 3>& normal) const;
+        FixedArray<SceneDir, 3>& normal) const override;
     virtual bool touches(
         const IIntersectable& intersectable,
         const TransformationMatrix<SceneDir, ScenePos, 3>& trafo,
         ScenePos& overlap,
         FixedArray<ScenePos, 3>& intersection_point,
-        FixedArray<SceneDir, 3>& normal) const;
+        FixedArray<SceneDir, 3>& normal) const override;
     virtual bool can_spawn_at(
         const CollisionPolygonSphere<CompressedScenePos, 3>& t) const override;
     virtual bool can_spawn_at(
