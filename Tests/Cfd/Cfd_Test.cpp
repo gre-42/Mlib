736abba5e4abc1a6b4f68d93fab52cea1b451013
Marc Kramer <gre@die-optimisten.net>
2025-06-14 19:23:59 +0200

    Template CFD test

--- Tests/Cfd/Cfd_Test.cpp
+++ Tests/Cfd/Cfd_Test.cpp
@@ -6,12 +6,13 @@
 
 using namespace Mlib;
 
+template <class TModel>
 void test_fluid_subdomain() {
-    using T = float;
+    using T = TModel::type;
     auto frequency = (T)1 / 8;
     auto omega = 2.f * T(M_PI) * frequency;
-    auto center_velocity = FixedArray<T, 2>{(T)0.1, (T)0.2};
-    FluidSubdomain<LbmModelD2Q9<T>> cfd{{70u, 20u}};
+    auto center_velocity = FixedArray<T, 2>{(T)0.2, (T)0.2};
+    FluidSubdomain<TModel> cfd{{70u, 20u}};
     for (size_t i = 0; i < 50; ++i) {
         for (size_t y = 5; y < 10; ++y) {
             cfd.set_velocity_field({35u, y}, std::cos((T)i * omega) * center_velocity);
@@ -33,7 +34,7 @@ void test_fluid_subdomain() {
 int main(int argc, const char** argv) {
     enable_floating_point_exceptions();
     try {
-        test_fluid_subdomain();
+        test_fluid_subdomain<LbmModelD2Q9<float>>();
     } catch (const std::runtime_error& e) {
         lerr() << e.what();
         return 1;
