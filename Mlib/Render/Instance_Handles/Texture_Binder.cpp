72c378da2a604f2a7d7c6acf6210155e198fdd08
Marc Kramer <gre@die-optimisten.net>
2025-06-22 14:03:13 +0200

    Set wrap mode in TextureBinder

--- Mlib/Render/Instance_Handles/Texture_Binder.cpp
+++ Mlib/Render/Instance_Handles/Texture_Binder.cpp
@@ -2,6 +2,7 @@
 #include <Mlib/Geometry/Material/Mipmap_Mode.hpp>
 #include <Mlib/Geometry/Texture/ITexture_Handle.hpp>
 #include <Mlib/Render/CHK.hpp>
+#include <Mlib/Render/Instance_Handles/Wrap_Mode.hpp>
 #include <Mlib/Throw_Or_Abort.hpp>
 
 using namespace Mlib;
@@ -16,14 +17,18 @@ void TextureBinder::bind(GLint uniform, const ITextureHandle& handle) {
     }
     CHK(glUniform1i(uniform, slot_));
     CHK(glActiveTexture(GL_TEXTURE0 + slot_));
+    GLenum target;
     if (handle.layers() > 1) {
         if (handle.mipmap_mode() == MipmapMode::WITH_MIPMAPS_2D) {
-            CHK(glBindTexture(GL_TEXTURE_2D_ARRAY, handle.handle<GLuint>()));
+            target = GL_TEXTURE_2D_ARRAY;
         } else {
-            CHK(glBindTexture(GL_TEXTURE_3D, handle.handle<GLuint>()));
+            target = GL_TEXTURE_3D;
         }
     } else {
-        CHK(glBindTexture(GL_TEXTURE_2D, handle.handle<GLuint>()));
+        target = GL_TEXTURE_2D;
     }
+    CHK(glBindTexture(target, handle.handle<GLuint>()));
+    CHK(glTexParameteri(target, GL_TEXTURE_WRAP_S, wrap_mode_to_native(handle.wrap_modes(0))));
+    CHK(glTexParameteri(target, GL_TEXTURE_WRAP_T, wrap_mode_to_native(handle.wrap_modes(1))));
     ++slot_;
 }
