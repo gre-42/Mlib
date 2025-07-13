61682db9b8ce43d27d66b4fb651df673cefea46e
Marc Kramer <gre@die-optimisten.net>
2025-07-13 17:06:12 +0200

    Support alphas in "remove_degenerate_triangles"

--- Mlib/Geometry/Mesh/Cleanup/Remove_Degenerate_Triangles.cpp
+++ Mlib/Geometry/Mesh/Cleanup/Remove_Degenerate_Triangles.cpp
@@ -8,9 +8,15 @@ using namespace Mlib;
 
 template <class TPos>
 void Mlib::remove_degenerate_triangles(ColoredVertexArray<TPos>& cva) {
-    std::vector<FixedArray<ColoredVertex<TPos>, 3>> trimmed;
-    trimmed.reserve(cva.triangles.size());
-    for (const auto& tri : cva.triangles) {
+    std::vector<FixedArray<ColoredVertex<TPos>, 3>> triangles;
+    std::vector<FixedArray<float, 3>> alpha;
+    triangles.reserve(cva.triangles.size());
+    alpha.reserve(cva.alpha.size());
+    if (!cva.alpha.empty() && (cva.alpha.size() != cva.triangles.size())) {
+        THROW_OR_ABORT("Conflicting number of alphas");
+    }
+    for (size_t i = 0; i < cva.triangles.size(); ++i) {
+        const auto& tri = cva.triangles[i];
         using Triangle = FixedArray<TPos, 3, 3>;
         auto tlc = triangle_largest_cosine(funpack(Triangle{
             tri(0).position,
@@ -19,9 +25,13 @@ void Mlib::remove_degenerate_triangles(ColoredVertexArray<TPos>& cva) {
         if (std::isnan(tlc) || (tlc > (1 - 1e-7f))) {
             continue;
         }
-        trimmed.push_back(tri);
+        triangles.push_back(tri);
+        if (!cva.alpha.empty()) {
+            alpha.push_back(cva.alpha[i]);
+        }
     }
-    cva.triangles = uuvector(trimmed.begin(), trimmed.end());
+    cva.triangles = uuvector(triangles.begin(), triangles.end());
+    cva.alpha = uuvector(alpha.begin(), alpha.end());
 }
 
 template void Mlib::remove_degenerate_triangles<float>(ColoredVertexArray<float>& cva);
