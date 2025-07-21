629264e9b09d21962d04ef599d88c49120da0b33
Marc Kramer <gre@die-optimisten.net>
2025-07-21 16:35:41 +0200

    Parse node_object/triangles

--- Mlib/Geometry/Material/Aggregate_Mode.cpp
+++ Mlib/Geometry/Material/Aggregate_Mode.cpp
@@ -11,6 +11,8 @@ AggregateMode Mlib::aggregate_mode_from_string(const std::string& str) {
         {"sorted", AggregateMode::SORTED_CONTINUOUSLY},
         {"instances_once", AggregateMode::INSTANCES_ONCE},
         {"instances_sorted", AggregateMode::INSTANCES_SORTED_CONTINUOUSLY},
+        {"node_object", AggregateMode::NODE_OBJECT},
+        {"node_triangles", AggregateMode::NODE_TRIANGLES},
         {"object_mask", AggregateMode::OBJECT_MASK},
         {"instances_mask", AggregateMode::INSTANCES_MASK}};
     auto it = m.find(str);
@@ -22,24 +24,15 @@ AggregateMode Mlib::aggregate_mode_from_string(const std::string& str) {
 
 std::string Mlib::aggregate_mode_to_string(AggregateMode aggregate_mode) {
     switch (aggregate_mode) {
-    case AggregateMode::NONE:
-        return "none";
-    case AggregateMode::ONCE:
-        return "once";
-    case AggregateMode::SORTED_CONTINUOUSLY:
-        return "sorted";
-    case AggregateMode::INSTANCES_ONCE:
-        return "instances_once";
-    case AggregateMode::INSTANCES_SORTED_CONTINUOUSLY:
-        return "instances_sorted";
-    case AggregateMode::NODE_OBJECT:
-        return "node_object";
-    case AggregateMode::NODE_TRIANGLES:
-        return "node_triangles";
-    case AggregateMode::OBJECT_MASK:
-        return "object_mask";
-    case AggregateMode::INSTANCES_MASK:
-        return "instances_mask";
+    case AggregateMode::NONE: return "none";
+    case AggregateMode::ONCE: return "once";
+    case AggregateMode::SORTED_CONTINUOUSLY: return "sorted";
+    case AggregateMode::INSTANCES_ONCE: return "instances_once";
+    case AggregateMode::INSTANCES_SORTED_CONTINUOUSLY: return "instances_sorted";
+    case AggregateMode::NODE_OBJECT: return "node_object";
+    case AggregateMode::NODE_TRIANGLES: return "node_triangles";
+    case AggregateMode::OBJECT_MASK: return "object_mask";
+    case AggregateMode::INSTANCES_MASK: return "instances_mask";
     }
     THROW_OR_ABORT("Unknown aggregate mode: \"" + std::to_string((int)aggregate_mode) + '"');
 }
