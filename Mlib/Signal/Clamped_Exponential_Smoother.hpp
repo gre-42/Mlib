0bc78726f26d40f96f52e8ff1457c64edb9c3336
Marc Kramer <gre@die-optimisten.net>
2025-09-14 19:11:32 +0200

    Cleanup ClampedExponentialSmoother

--- Mlib/Signal/Clamped_Exponential_Smoother.hpp
+++ Mlib/Signal/Clamped_Exponential_Smoother.hpp
@@ -18,36 +18,23 @@ public:
         : alpha_{ alpha }
         , interval_{ interval }
     {}
-    ClampedExponentialSmoother(const TFloat& alpha, const Interval<TData>& interval, const TData& x0)
-        : alpha_{ alpha }
-        , interval_{ interval }
-        , s_{ x0 }
-    {}
     template <class TX0>
-    ClampedExponentialSmoother(const TFloat& alpha, const Interval<TData>& interval, const TX0& x0, const TFloat& dt)
-        : alpha_{ std::pow(alpha, dt) }
+    ClampedExponentialSmoother(const TFloat& alpha, const Interval<TData>& interval, const TX0& x0)
+        : alpha_{ alpha }
         , interval_{ interval }
         , s_{ x0 }
     {}
     const TData& operator () (const TData& x) {
         if (s_.has_value()) {
-            *s_ = std::clamp(lerp(*s_, x, alpha_), *s_ + interval_.min, *s_ + interval_.max);
-        } else {
-            s_ = x;
-        }
-        return *s_;
-    }
-    const TData& operator () (const TData& x, const TFloat& dt) {
-        if (s_.has_value()) {
-            *s_ = std::clamp(lerp(*s_, x, std::pow(alpha_, 1 / dt)), *s_ + interval_.min, *s_ + interval_.max);
+            *s_ += std::clamp(lerp(*s_, x, alpha_) - *s_, interval_.min, interval_.max);
         } else {
             s_ = x;
         }
         return *s_;
     }
     ClampedExponentialSmoother changed_time_step(const TFloat& from, const TFloat& to) const {
-        auto f = from / to;
-        return { alpha_, s_, f };
+        auto f = to / from;
+        return { std::pow(alpha_, 1 / f), {interval_.min * f, interval_.max * f}, s_ };
     }
     const std::optional<TData>& xhat() const {
         return s_;
