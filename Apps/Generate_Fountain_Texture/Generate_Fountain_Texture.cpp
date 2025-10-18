5a9a808254c70d93d16a1f9b6a2cc907a29b4a77
Marc Kramer <gre@die-optimisten.net>
2025-10-18 15:33:13 +0200

    Fade-out fountain

--- Apps/Generate_Fountain_Texture/Generate_Fountain_Texture.cpp
+++ Apps/Generate_Fountain_Texture/Generate_Fountain_Texture.cpp
@@ -35,13 +35,14 @@ int main(int argc, char** argv) {
             safe_stof(args.named_value("-r", "1")),
             safe_stof(args.named_value("-g", "1")),
             safe_stof(args.named_value("-b", "1"))};
-        size_t nparticles = 100;
+        size_t nparticles = 50;
         float initial_velocity_mean = 400 * cm / seconds;
-        float initial_velocity_std = 100 * cm / seconds;
+        float initial_velocity_std = 300 * cm / seconds;
         float gravity = 9.8f * meters / squared(seconds);
         float dpi = 96.f / meters;
         float particle_sigma = 9 * cm;
         float intensity_scaling = 1.f;
+        float fadeout_sigma = 60 * cm;
         float dt = 4.f / 60.f * seconds;
         std::vector<VectorAtPosition<float, float, 2>> particles(
             nparticles,
@@ -53,14 +54,14 @@ int main(int argc, char** argv) {
                 p.vector = velocity_direction_rng() * velocity_magnitued_rng();
             }
         }
-        auto kernel1d = gaussian_kernel(particle_sigma * dpi, 4.f);
-        kernel1d /= max(kernel1d);
+        auto kernel1d = gaussian_kernel(particle_sigma * dpi, 4.f, ShapeType::FULL, NormalizationType::MAX);
         auto kernel = Array<float>{ArrayShape{ kernel1d.length(), kernel1d.length() }};
         for (size_t r = 0; r < kernel1d.length(); ++r) {
             for (size_t c = 0; c < kernel1d.length(); ++c) {
                 kernel(r, c) = kernel1d(r) * kernel1d(c);
             }
         }
+        auto fadeout_kernel1d = gaussian_kernel(fadeout_sigma * dpi, 4.f, ShapeType::HALF, NormalizationType::MAX);
         linfo() << "Kernel size: " << kernel.shape();
         for (size_t i = 0; i < nimages; ++i) {
             out_alpha = 0.f;
@@ -78,6 +79,18 @@ int main(int argc, char** argv) {
                             continue;
                         }
                         out_alpha(rti, cti) += kernel(rs, cs);
+                    }
+                }
+            }
+            for (size_t r = 0; r < out_alpha.shape(0); ++r) {
+                for (size_t c = 0; c < out_alpha.shape(1); ++c) {
+                    auto i = float_to_integral<size_t>(std::round(std::sqrt(
+                        squared((double)r - (double)out_alpha.shape(0) / 2) +
+                        squared((double)c - (double)out_alpha.shape(1) / 2))));
+                    if (i >= fadeout_kernel1d.length()) {
+                        out_alpha(r, c) = 0.f;
+                    } else {
+                        out_alpha(r, c) *= fadeout_kernel1d(i);
                     }
                 }
             }
--- Mlib/Images/Filters/Gaussian_Kernel.hpp
+++ Mlib/Images/Filters/Gaussian_Kernel.hpp
@@ -1,19 +1,53 @@
 #pragma once
 #include <Mlib/Array/Array.hpp>
 #include <Mlib/Memory/Float_To_Integral.hpp>
+#include <Mlib/Stats/Min_Max.hpp>
 
 namespace Mlib {
 
+enum class ShapeType {
+    HALF,
+    FULL
+};
+
+enum class NormalizationType {
+    SUM,
+    MAX
+};
+
 template <class TSigma>
-Array<TSigma> gaussian_kernel(const TSigma& sigma, const TSigma& truncate) {
-    Array<TSigma> coeffs(ArrayShape{{1 + 2 * float_to_integral<size_t>(std::round(truncate * sigma))}});
-    size_t cdist = coeffs.length() / 2;
-    for (size_t i = cdist; i < coeffs.length(); ++i) {
-        coeffs(i) = std::exp(-squared(TSigma(i - cdist) / sigma) / 2);
-        coeffs(coeffs.length() - i - 1) = coeffs(i);
+Array<TSigma> gaussian_kernel(
+    const TSigma& sigma,
+    const TSigma& truncate,
+    ShapeType shape = ShapeType::FULL,
+    NormalizationType normalization = NormalizationType::SUM)
+{
+    Array<TSigma> coeffs;
+    if (shape == ShapeType::HALF) {
+        coeffs = Array<TSigma>(ArrayShape{{1 + float_to_integral<size_t>(std::round(truncate * sigma))}});
+        for (size_t i = 0; i < coeffs.length(); ++i) {
+            coeffs(i) = std::exp(-squared(TSigma(i) / sigma) / 2);
+        }
+    } else if (shape == ShapeType::FULL) {
+        coeffs = Array<TSigma>(ArrayShape{{1 + 2 * float_to_integral<size_t>(std::round(truncate * sigma))}});
+        size_t cdist = coeffs.length() / 2;
+        for (size_t i = cdist; i < coeffs.length(); ++i) {
+            coeffs(i) = std::exp(-squared(TSigma(i - cdist) / sigma) / 2);
+            coeffs(coeffs.length() - i - 1) = coeffs(i);
+        }
     }
-    coeffs /= sum(coeffs);
-    return coeffs;
+    if (!coeffs.initialized()) {
+        THROW_OR_ABORT("Unknown kernel shape type");
+    }
+    switch (normalization) {
+    case NormalizationType::SUM:
+        coeffs /= sum(coeffs);
+        return coeffs;
+    case NormalizationType::MAX:
+        coeffs /= max(coeffs);
+        return coeffs;
+    }
+    THROW_OR_ABORT("Unknown normalization type");
 }
 
 }
