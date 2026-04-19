6fcfe96d01d3d36a32ead2c726349292c64c6427
Marc Kramer <gre@die-optimisten.net>
2026-04-19 10:09:58 +0200

    Update OpenSSL

--- install_openssl.ps1
+++ install_openssl.ps1
@@ -1,14 +1,13 @@
 Push-Location $PSScriptRoot
 
-# Disabled because the OpenSSL version was outdated (07.08.2025)
-# choco install openssl -y
+choco install openssl -y
 
-Write-Host "Download Win64 OpenSSL"
-Invoke-WebRequest https://slproweb.com/download/Win64OpenSSL-3_5_2.exe -OutFile Win64OpenSSL-3_5_2.exe
-
-Write-Host "Install Win64 OpenSSL"
-# From: https://community.chocolatey.org/packages/openssl#files
-.\Win64OpenSSL-3_5_2.exe /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
-Write-Host "Finished installation of Win64 OpenSSL"
+# Write-Host "Download Win64 OpenSSL"
+# Invoke-WebRequest https://slproweb.com/download/Win64OpenSSL-4_0_0.exe -OutFile Win64OpenSSL-4_0_0.exe
+# 
+# Write-Host "Install Win64 OpenSSL"
+# # From: https://community.chocolatey.org/packages/openssl#files
+# .\Win64OpenSSL-4_0_0.exe /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-
+# Write-Host "Finished installation of Win64 OpenSSL"
 
 Pop-Location
--- package_scene_renderer.ps1
+++ package_scene_renderer.ps1
@@ -21,17 +21,15 @@ Write-Host "OpenSSL path: $openssl_path"
 
 $files = "openal-soft\build\Release\OpenAL32.dll",
          "freealut\build\src\Release\alut.dll",
-         "$openssl_path\libssl-3-x64.dll",
-         "$openssl_path\libcrypto-3-x64.dll",
          "VSRecastBuild\Detour\$CMAKE_BUILD_TYPE\Detour.dll",
          "VSRecastBuild\DebugUtils\$CMAKE_BUILD_TYPE\DebugUtils.dll",
          "VSRecastBuild\Recast\$CMAKE_BUILD_TYPE\Recast.dll",
          "VSZlibBuild\Release\zlib.dll",
          "glfw_vc2022\lib\glfw3.dll"
 
-$icu_dlls = (Get-ChildItem -Path "$env:VCPKG_INSTALLATION_ROOT\installed\x64-windows\bin\icu*.dll" -Exclude "icuio*.dll").FullName
-
-$files += $icu_dlls
+$files += (Get-ChildItem -Path "$openssl_path\libssl-*-x64.dll").FullName
+$files += (Get-ChildItem -Path "$openssl_path\libcrypto-*-x64.dll").FullName
+$files += (Get-ChildItem -Path "$env:VCPKG_INSTALLATION_ROOT\installed\x64-windows\bin\icu*.dll" -Exclude "icuio*.dll").FullName
 
 $dest_dir = "${BUILD_PREFIX}GVS\Bin\$CMAKE_BUILD_TYPE\"
 foreach ($file in $files) {
