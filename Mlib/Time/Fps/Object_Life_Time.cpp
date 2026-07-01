3b32f7a4bfb87e2c8919f3939fbb4d70f541ce3a
Marc Kramer <gre@die-optimisten.net>
2026-07-01 22:57:27 +0200

    PEP8

--- Mlib/Time/Fps/Object_Life_Time.cpp
+++ Mlib/Time/Fps/Object_Life_Time.cpp
@@ -3,14 +3,14 @@
 using namespace Mlib;
 
 ObjectLifeTime::ObjectLifeTime(std::function<void(std::chrono::steady_clock::duration)> func)
-	: func_{ std::move(func) }
-	, start_time_{ std::chrono::steady_clock::now() }
+    : func_{ std::move(func) }
+    , start_time_{ std::chrono::steady_clock::now() }
 {}
 
 ObjectLifeTime::~ObjectLifeTime() {
-	func_(elapsed());
+    func_(elapsed());
 }
 
 std::chrono::steady_clock::duration ObjectLifeTime::elapsed() const {
-	return std::chrono::steady_clock::now() - start_time_;
+    return std::chrono::steady_clock::now() - start_time_;
 }
--- Mlib/Time/Fps/Object_Life_Time.hpp
+++ Mlib/Time/Fps/Object_Life_Time.hpp
@@ -6,12 +6,12 @@ namespace Mlib {
 
 class ObjectLifeTime {
 public:
-	explicit ObjectLifeTime(std::function<void(std::chrono::steady_clock::duration elapsed)> func);
-	~ObjectLifeTime();
-	std::chrono::steady_clock::duration elapsed() const;
+    explicit ObjectLifeTime(std::function<void(std::chrono::steady_clock::duration elapsed)> func);
+    ~ObjectLifeTime();
+    std::chrono::steady_clock::duration elapsed() const;
 private:
-	std::function<void(std::chrono::steady_clock::duration elapsed)> func_;
-	std::chrono::steady_clock::time_point start_time_;
+    std::function<void(std::chrono::steady_clock::duration elapsed)> func_;
+    std::chrono::steady_clock::time_point start_time_;
 };
 
 }
