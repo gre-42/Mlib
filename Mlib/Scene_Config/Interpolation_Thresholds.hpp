c760dea3744a7a790b8a09fea5267dfe1a59c7fd
Marc Kramer <gre@die-optimisten.net>
2026-07-22 23:34:18 +0200

    Decrease interpolation halflife

--- Mlib/Scene_Config/Interpolation_Thresholds.hpp
+++ Mlib/Scene_Config/Interpolation_Thresholds.hpp
@@ -5,6 +5,6 @@ namespace Mlib {
 
 static constexpr const float INTERPOLATION_ERROR_DISTANCE = 0.5f * seconds * 200.f * kph;
 static constexpr const float REMOTE_INTERPOLATION_JUMP_DISTANCE = 0.2f * seconds * 200.f * kph;
-static constexpr const float REMOTE_INTERPOLATION_HALFLIFE = 0.1f * seconds;
+static constexpr const float REMOTE_INTERPOLATION_HALFLIFE = 0.05f * seconds;
 
 }
