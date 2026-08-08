f680c24ff54d0491338bdd440569a51d420e630e
Marc Kramer <gre@die-optimisten.net>
2026-08-08 12:36:56 +0200

    Increase remote interpolation jump distance (for the remote missile0)

--- Mlib/Scene_Config/Interpolation_Thresholds.hpp
+++ Mlib/Scene_Config/Interpolation_Thresholds.hpp
@@ -3,8 +3,8 @@
 
 namespace Mlib {
 
-static constexpr const float INTERPOLATION_ERROR_DISTANCE = 0.5f * seconds * 200.f * kph;
-static constexpr const float REMOTE_INTERPOLATION_JUMP_DISTANCE = 0.2f * seconds * 200.f * kph;
+static constexpr const float INTERPOLATION_ERROR_DISTANCE = 1.f * seconds * 200.f * kph;
+static constexpr const float REMOTE_INTERPOLATION_JUMP_DISTANCE = 0.4f * seconds * 200.f * kph;
 static constexpr const float REMOTE_INTERPOLATION_HALFLIFE = 0.05f * seconds;
 static constexpr const float REMOTE_AIR_DAMPING_HALFLIFE = 0.2f * seconds;
 
