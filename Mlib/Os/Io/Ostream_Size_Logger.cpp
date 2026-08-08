d403fd5e513e17b9001b631d103709f74b41fd06
Marc Kramer <gre@die-optimisten.net>
2026-08-08 10:58:36 +0200

    Cleanup OstreamSizeLogger

--- Mlib/Os/Io/Ostream_Size_Logger.cpp
+++ Mlib/Os/Io/Ostream_Size_Logger.cpp
@@ -8,13 +8,10 @@ using namespace Mlib;
 OstreamSizeLogger::OstreamSizeLogger(std::ostream& ostr, std::string message)
     : ostr_{ostr}
     , message_{std::move(message)}
-{
-    ostr.seekp(0, std::ios::end);
-    streampos_ = ostr.tellp();
-}
+    , streampos_{ostr.tellp()}
+{}
 
 OstreamSizeLogger::~OstreamSizeLogger() {
-    ostr_.seekp(0, std::ios::end);
     auto len = integral_cast<size_t>(ostr_.tellp() - streampos_);
     linfo() << message_ << len;
 }
